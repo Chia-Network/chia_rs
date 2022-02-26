32f60c18a645c885a37a7e43223cff4f8179fe1a
Richard Kiss <him@richardkiss.com>
2022-02-25 19:18:03 -0800

    Remove recursion when parsing `ProgramArray`.

--- src/streamable/program.rs
+++ src/streamable/program.rs
@@ -19,58 +19,59 @@ impl<'de> Deserialize<'de> for ProgramArray {
         D: Deserializer<'de>,
     {
         let visitor = ProgramArrayVisitor {};
-        deserializer.deserialize_tuple(35, visitor)
+        deserializer.deserialize_tuple(1024*1024*1024, visitor)
     }
 }
 
 fn size_for_initial_byte<'de, S>(
     initial_byte: u8,
     seq: &mut S,
-) -> Result<(usize, Vec<u8>), S::Error>
+    vec: &mut Vec<u8>,
+) -> Result<usize, S::Error>
 where
     S: SeqAccess<'de>,
 {
     let mut size: usize = 0;
-    let mut size_blob: Vec<u8> = Vec::new();
     if initial_byte & 0xc0 == 0x80 {
         size = (initial_byte & 0x7f) as usize;
     }
     if initial_byte & 0xe0 == 0xc0 {
         let size_byte: [u8; 1] = seq.next_element()?.unwrap();
         size = (((initial_byte as usize) & 0x3f) << 8) | (size_byte[0] as usize);
-        size_blob = size_byte.into();
+        vec.extend_from_slice(&size_byte);
     }
     if initial_byte & 0xf0 == 0xe0 {
         let size_byte: [u8; 2] = seq.next_element()?.unwrap();
         size = (((initial_byte as usize) & 0x1f) << 16)
             | ((size_byte[0] as usize) << 8)
             | (size_byte[1] as usize);
-        size_blob = size_byte.into();
+        vec.extend_from_slice(&size_byte);
     }
-    Ok((size, size_blob))
+
+    Ok(size)
 }
 
-fn blob_for_size<'de, S>(mut size: usize, seq: &mut S) -> Result<Vec<u8>, S::Error>
+fn blob_for_size<'de, S>(mut size: usize, seq: &mut S, vec: &mut Vec<u8>) -> Result<(), S::Error>
 where
     S: SeqAccess<'de>,
 {
-    let mut final_blob: Vec<u8> = Vec::new();
-    while size > 32 {
+    vec.reserve(size);
+    while size >= 32 {
         let b: [u8; 32] = seq.next_element()?.unwrap();
-        final_blob.extend_from_slice(&b);
+        vec.extend_from_slice(&b);
         size -= 32;
     }
-    while size > 8 {
+    while size >= 8 {
         let b: [u8; 8] = seq.next_element()?.unwrap();
-        final_blob.extend_from_slice(&b);
+        vec.extend_from_slice(&b);
         size -= 8;
     }
-    while size > 0 {
+    while size >= 1 {
         let b: [u8; 1] = seq.next_element()?.unwrap();
-        final_blob.extend_from_slice(&b);
+        vec.extend_from_slice(&b);
         size -= 1;
     }
-    Ok(final_blob)
+    Ok(())
 }
 
 struct ProgramArrayVisitor {}
@@ -81,23 +82,20 @@ impl<'de> Visitor<'de> for ProgramArrayVisitor {
     where
         S: SeqAccess<'de>,
     {
-        let initial_byte: u8 = seq.next_element()?.unwrap();
-        let mut r = vec![initial_byte];
-        if initial_byte == 0xff {
-            let left: ProgramArray = seq.next_element()?.unwrap();
-            let right: ProgramArray = seq.next_element()?.unwrap();
-            r.extend_from_slice(&left.0);
-            r.extend_from_slice(&right.0);
-            return Ok(ProgramArray(r));
+        let mut node_count = 1;
+        let mut r = Vec::with_capacity(256);
+        while node_count > 0 {
+            node_count -= 1;
+            let initial_byte: u8 = seq.next_element()?.unwrap();
+            r.push(initial_byte);
+            if initial_byte == 0xff {
+                node_count += 2;
+            } else if initial_byte & 0x80 != 0 {
+                let size = size_for_initial_byte(initial_byte, &mut seq, &mut r)?;
+                blob_for_size(size, &mut seq, &mut r)?;
+            }
         }
-        if initial_byte & 0x80 == 0 {
-            return Ok(ProgramArray(vec![initial_byte]));
-        }
-        let (size, size_blob) = size_for_initial_byte(initial_byte, &mut seq)?;
-        let blob = blob_for_size(size, &mut seq)?;
-
-        r.extend_from_slice(&size_blob);
-        r.extend_from_slice(&blob);
+        r.shrink_to_fit();
         let r = ProgramArray(r);
         Ok(r)
     }
