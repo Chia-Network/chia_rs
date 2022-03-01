22999830265a90a2caa85f3f99e2f6c841dc7ed6
Richard Kiss <him@richardkiss.com>
2022-02-28 18:01:53 -0800

    Remove `LazyNode`.

--- wheel/src/adapt_response.rs
+++ wheel/src/adapt_response.rs
@@ -1,25 +1,19 @@
-use crate::lazy_node::LazyNode;
 use clvmr::allocator::Allocator;
+use clvmr::node::Node;
 use clvmr::reduction::EvalErr;
-
-use std::rc::Rc;
+use clvmr::serialize::node_to_bytes;
 
 use pyo3::prelude::*;
 use pyo3::types::PyDict;
 
 pub fn eval_err_to_pyresult<T>(py: Python, eval_err: EvalErr, allocator: Allocator) -> PyResult<T> {
-    let node = LazyNode::new(Rc::new(allocator), eval_err.0);
+    let node = Node::new(&allocator, eval_err.0);
+    let blob = node_to_bytes(&node).unwrap();
     let msg = eval_err.1;
     let ctx: &PyDict = PyDict::new(py);
     ctx.set_item("msg", msg)?;
-    ctx.set_item("node", node)?;
+    ctx.set_item("blob", blob)?;
     Err(py
-        .run(
-            "
-from clvm.EvalError import EvalError
-raise EvalError(msg, node)",
-            None,
-            Some(ctx),
-        )
+        .run("raise ValueError(msg, bytes(blob).hex())", None, Some(ctx))
         .unwrap_err())
 }
--- wheel/src/lib.rs
+++ wheel/src/lib.rs
@@ -1,4 +1,3 @@
 mod adapt_response;
 mod api;
-mod lazy_node;
 mod run_generator;
