14eca4c5d354fea7b1ead9b1f9b382cea4a28171
arvidn <arvid@libtorrent.org>
2022-07-29 13:13:15 -0700

    remove RangeSet, now that redundant leading zeros are invalid in conditions, there's no need to jump through hoops to support malicious programs

--- src/gen/condition_sanitizers.rs
+++ src/gen/condition_sanitizers.rs
@@ -1,4 +1,3 @@
-use super::rangeset::RangeSet;
 use super::sanitize_int::sanitize_uint;
 use super::validation_error::{atom, ErrorCode, ValidationErr};
 use clvmr::allocator::{Allocator, NodePtr};
@@ -19,15 +18,9 @@ pub fn sanitize_hash(
     }
 }
 
-pub fn parse_amount(
-    a: &Allocator,
-    n: NodePtr,
-    code: ErrorCode,
-    range_cache: &mut RangeSet,
-    flags: u32,
-) -> Result<u64, ValidationErr> {
+pub fn parse_amount(a: &Allocator, n: NodePtr, code: ErrorCode) -> Result<u64, ValidationErr> {
     // amounts are not allowed to exceed 2^64. i.e. 8 bytes
-    match sanitize_uint(a, n, 8, code, range_cache, flags) {
+    match sanitize_uint(a, n, 8, code) {
         Err(ValidationErr(n, ErrorCode::NegativeAmount)) => Err(ValidationErr(n, code)),
         Err(r) => Err(r),
         Ok(r) => Ok(u64_from_bytes(r)),
@@ -36,15 +29,9 @@ pub fn parse_amount(
 
 // a negative height is always true. In this case the
 // condition can be ignored and this functon returns 0
-pub fn parse_height(
-    a: &Allocator,
-    n: NodePtr,
-    code: ErrorCode,
-    range_cache: &mut RangeSet,
-    flags: u32,
-) -> Result<u32, ValidationErr> {
+pub fn parse_height(a: &Allocator, n: NodePtr, code: ErrorCode) -> Result<u32, ValidationErr> {
     // heights are not allowed to exceed 2^32. i.e. 4 bytes
-    match sanitize_uint(a, n, 4, code, range_cache, flags) {
+    match sanitize_uint(a, n, 4, code) {
         // Height is always positive, so a negative requirement is always true,
         // just like 0.
         Err(ValidationErr(_, ErrorCode::NegativeAmount)) => Ok(0),
@@ -54,15 +41,9 @@ pub fn parse_height(
 }
 
 // negative seconds are always valid conditions, and will return 0
-pub fn parse_seconds(
-    a: &Allocator,
-    n: NodePtr,
-    code: ErrorCode,
-    range_cache: &mut RangeSet,
-    flags: u32,
-) -> Result<u64, ValidationErr> {
+pub fn parse_seconds(a: &Allocator, n: NodePtr, code: ErrorCode) -> Result<u64, ValidationErr> {
     // seconds are not allowed to exceed 2^64. i.e. 8 bytes
-    match sanitize_uint(a, n, 8, code, range_cache, flags) {
+    match sanitize_uint(a, n, 8, code) {
         // seconds is always positive, so a negative requirement is always true,
         // we don't need to include this condition
         Err(ValidationErr(_, ErrorCode::NegativeAmount)) => Ok(0),
@@ -84,9 +65,6 @@ pub fn sanitize_announce_msg(
         Ok(n)
     }
 }
-
-#[cfg(test)]
-use crate::gen::flags::COND_CANON_INTS;
 
 #[cfg(test)]
 fn zero_vec(len: usize) -> Vec<u8> {
@@ -152,250 +130,180 @@ fn test_sanitize_announce_msg() {
 }
 
 #[cfg(test)]
-fn amount_tester(buf: &[u8], flags: u32) -> Result<u64, ValidationErr> {
+fn amount_tester(buf: &[u8]) -> Result<u64, ValidationErr> {
     let mut a = Allocator::new();
     let n = a.new_atom(buf).unwrap();
-    let mut range_cache = RangeSet::new();
 
-    parse_amount(
-        &mut a,
-        n,
-        ErrorCode::InvalidCoinAmount,
-        &mut range_cache,
-        flags,
-    )
+    parse_amount(&mut a, n, ErrorCode::InvalidCoinAmount)
 }
 
 #[test]
 fn test_sanitize_amount() {
-    for flags in &[0, COND_CANON_INTS] {
-        // negative amounts are not allowed
-        // regardless of flags
-        assert_eq!(
-            amount_tester(&[0x80], *flags).unwrap_err().1,
-            ErrorCode::InvalidCoinAmount
-        );
-        assert_eq!(
-            amount_tester(&[0xff], *flags).unwrap_err().1,
-            ErrorCode::InvalidCoinAmount
-        );
-        assert_eq!(
-            amount_tester(&[0xff, 0], *flags).unwrap_err().1,
-            ErrorCode::InvalidCoinAmount
-        );
+    // negative amounts are not allowed
+    // regardless of flags
+    assert_eq!(
+        amount_tester(&[0x80]).unwrap_err().1,
+        ErrorCode::InvalidCoinAmount
+    );
+    assert_eq!(
+        amount_tester(&[0xff]).unwrap_err().1,
+        ErrorCode::InvalidCoinAmount
+    );
+    assert_eq!(
+        amount_tester(&[0xff, 0]).unwrap_err().1,
+        ErrorCode::InvalidCoinAmount
+    );
 
-        // leading zeros are somtimes necessary to make values positive
-        assert_eq!(amount_tester(&[0, 0xff], *flags), Ok(0xff));
-        // but are stripped when they are redundant
-        if (flags & COND_CANON_INTS) != 0 {
-            assert_eq!(
-                amount_tester(&[0, 0, 0, 0xff], *flags).unwrap_err().1,
-                ErrorCode::InvalidCoinAmount
-            );
-            assert_eq!(
-                amount_tester(&[0, 0, 0, 0x80], *flags).unwrap_err().1,
-                ErrorCode::InvalidCoinAmount
-            );
-            assert_eq!(
-                amount_tester(&[0, 0, 0, 0x7f], *flags).unwrap_err().1,
-                ErrorCode::InvalidCoinAmount
-            );
-            assert_eq!(
-                amount_tester(&[0, 0, 0], *flags).unwrap_err().1,
-                ErrorCode::InvalidCoinAmount
-            );
-        } else {
-            assert_eq!(amount_tester(&[0, 0, 0, 0xff], *flags), Ok(0xff));
-            assert_eq!(amount_tester(&[0, 0, 0, 0x80], *flags), Ok(0x80));
-            assert_eq!(amount_tester(&[0, 0, 0, 0x7f], *flags), Ok(0x7f));
-            assert_eq!(amount_tester(&[0, 0, 0], *flags), Ok(0));
-        }
+    // leading zeros are somtimes necessary to make values positive
+    assert_eq!(amount_tester(&[0, 0xff]), Ok(0xff));
+    // but are stripped when they are redundant
+    assert_eq!(
+        amount_tester(&[0, 0, 0, 0xff]).unwrap_err().1,
+        ErrorCode::InvalidCoinAmount
+    );
+    assert_eq!(
+        amount_tester(&[0, 0, 0, 0x80]).unwrap_err().1,
+        ErrorCode::InvalidCoinAmount
+    );
+    assert_eq!(
+        amount_tester(&[0, 0, 0, 0x7f]).unwrap_err().1,
+        ErrorCode::InvalidCoinAmount
+    );
+    assert_eq!(
+        amount_tester(&[0, 0, 0]).unwrap_err().1,
+        ErrorCode::InvalidCoinAmount
+    );
 
-        // amounts aren't allowed to be too big
-        assert_eq!(
-            amount_tester(&[0x7f, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], *flags)
-                .unwrap_err()
-                .1,
-            ErrorCode::InvalidCoinAmount
-        );
+    // amounts aren't allowed to be too big
+    assert_eq!(
+        amount_tester(&[0x7f, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
+            .unwrap_err()
+            .1,
+        ErrorCode::InvalidCoinAmount
+    );
 
-        // this is small enough though
-        assert_eq!(
-            amount_tester(&[0, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff], *flags),
-            Ok(0xffffffffffffffff)
-        );
-    }
+    // this is small enough though
+    assert_eq!(
+        amount_tester(&[0, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff]),
+        Ok(0xffffffffffffffff)
+    );
 }
 
 #[cfg(test)]
-fn height_tester(buf: &[u8], flags: u32) -> Result<u32, ValidationErr> {
+fn height_tester(buf: &[u8]) -> Result<u32, ValidationErr> {
     let mut a = Allocator::new();
     let n = a.new_atom(buf).unwrap();
-    let mut range_cache = RangeSet::new();
 
-    parse_height(
-        &mut a,
-        n,
-        ErrorCode::AssertHeightAbsolute,
-        &mut range_cache,
-        flags,
-    )
+    parse_height(&mut a, n, ErrorCode::AssertHeightAbsolute)
 }
 
 #[test]
 fn test_parse_height() {
-    for flags in &[0, COND_CANON_INTS] {
-        // negative heights can be ignored
-        assert_eq!(height_tester(&[0x80], *flags), Ok(0));
-        assert_eq!(height_tester(&[0xff], *flags), Ok(0));
-        assert_eq!(height_tester(&[0xff, 0], *flags), Ok(0));
+    // negative heights can be ignored
+    assert_eq!(height_tester(&[0x80]), Ok(0));
+    assert_eq!(height_tester(&[0xff]), Ok(0));
+    assert_eq!(height_tester(&[0xff, 0]), Ok(0));
 
-        // leading zeros are somtimes necessary to make values positive
-        assert_eq!(height_tester(&[0, 0xff], *flags), Ok(0xff));
-        // but are stripped when they are redundant
-        if (flags & COND_CANON_INTS) != 0 {
-            assert_eq!(
-                height_tester(&[0, 0, 0, 0xff], *flags).unwrap_err().1,
-                ErrorCode::AssertHeightAbsolute
-            );
-            assert_eq!(
-                height_tester(&[0, 0, 0, 0x80], *flags).unwrap_err().1,
-                ErrorCode::AssertHeightAbsolute
-            );
-            assert_eq!(
-                height_tester(&[0, 0, 0, 0x7f], *flags).unwrap_err().1,
-                ErrorCode::AssertHeightAbsolute
-            );
-            assert_eq!(
-                height_tester(&[0, 0, 0], *flags).unwrap_err().1,
-                ErrorCode::AssertHeightAbsolute
-            );
-            assert_eq!(
-                height_tester(&[0], *flags).unwrap_err().1,
-                ErrorCode::AssertHeightAbsolute
-            );
-        } else {
-            assert_eq!(height_tester(&[0, 0, 0, 0xff], *flags), Ok(0xff));
-            assert_eq!(height_tester(&[0, 0, 0, 0x80], *flags), Ok(0x80));
-            assert_eq!(height_tester(&[0, 0, 0, 0x7f], *flags), Ok(0x7f));
-            assert_eq!(height_tester(&[0, 0, 0], *flags), Ok(0));
-            assert_eq!(height_tester(&[0], *flags), Ok(0));
-        }
+    // leading zeros are somtimes necessary to make values positive
+    assert_eq!(height_tester(&[0, 0xff]), Ok(0xff));
+    // but are stripped when they are redundant
+    assert_eq!(
+        height_tester(&[0, 0, 0, 0xff]).unwrap_err().1,
+        ErrorCode::AssertHeightAbsolute
+    );
+    assert_eq!(
+        height_tester(&[0, 0, 0, 0x80]).unwrap_err().1,
+        ErrorCode::AssertHeightAbsolute
+    );
+    assert_eq!(
+        height_tester(&[0, 0, 0, 0x7f]).unwrap_err().1,
+        ErrorCode::AssertHeightAbsolute
+    );
+    assert_eq!(
+        height_tester(&[0, 0, 0]).unwrap_err().1,
+        ErrorCode::AssertHeightAbsolute
+    );
+    assert_eq!(
+        height_tester(&[0]).unwrap_err().1,
+        ErrorCode::AssertHeightAbsolute
+    );
 
-        // heights aren't allowed to be > 2^32 (i.e. 5 bytes)
-        assert_eq!(
-            height_tester(&[0x7f, 0xff, 0xff, 0xff, 0xff, 0xff], *flags)
-                .unwrap_err()
-                .1,
-            ErrorCode::AssertHeightAbsolute
-        );
+    // heights aren't allowed to be > 2^32 (i.e. 5 bytes)
+    assert_eq!(
+        height_tester(&[0x7f, 0xff, 0xff, 0xff, 0xff, 0xff])
+            .unwrap_err()
+            .1,
+        ErrorCode::AssertHeightAbsolute
+    );
 
-        // this is small enough though
-        assert_eq!(
-            height_tester(&[0, 0xff, 0xff, 0xff, 0xff], *flags),
-            Ok(0xffffffff)
-        );
+    // this is small enough though
+    assert_eq!(height_tester(&[0, 0xff, 0xff, 0xff, 0xff]), Ok(0xffffffff));
 
-        let mut a = Allocator::new();
-        let pair = a.new_pair(a.null(), a.null()).unwrap();
-        let mut range_cache = RangeSet::new();
-        assert_eq!(
-            parse_height(
-                &mut a,
-                pair,
-                ErrorCode::AssertHeightAbsolute,
-                &mut range_cache,
-                *flags
-            ),
-            Err(ValidationErr(pair, ErrorCode::AssertHeightAbsolute))
-        );
-    }
+    let mut a = Allocator::new();
+    let pair = a.new_pair(a.null(), a.null()).unwrap();
+    assert_eq!(
+        parse_height(&mut a, pair, ErrorCode::AssertHeightAbsolute),
+        Err(ValidationErr(pair, ErrorCode::AssertHeightAbsolute))
+    );
 }
 
 #[cfg(test)]
-fn seconds_tester(buf: &[u8], flags: u32) -> Result<u64, ValidationErr> {
+fn seconds_tester(buf: &[u8]) -> Result<u64, ValidationErr> {
     let mut a = Allocator::new();
     let n = a.new_atom(buf).unwrap();
-    let mut range_cache = RangeSet::new();
 
-    parse_seconds(
-        &mut a,
-        n,
-        ErrorCode::AssertSecondsAbsolute,
-        &mut range_cache,
-        flags,
-    )
+    parse_seconds(&mut a, n, ErrorCode::AssertSecondsAbsolute)
 }
 
 #[test]
 fn test_parse_seconds() {
-    for flags in &[0, COND_CANON_INTS] {
-        // negative seconds can be ignored
-        assert_eq!(seconds_tester(&[0x80], *flags), Ok(0));
-        assert_eq!(seconds_tester(&[0xff], *flags), Ok(0));
-        assert_eq!(seconds_tester(&[0xff, 0], *flags), Ok(0));
+    // negative seconds can be ignored
+    assert_eq!(seconds_tester(&[0x80]), Ok(0));
+    assert_eq!(seconds_tester(&[0xff]), Ok(0));
+    assert_eq!(seconds_tester(&[0xff, 0]), Ok(0));
 
-        // leading zeros are somtimes necessary to make values positive
-        assert_eq!(seconds_tester(&[0, 0xff], *flags), Ok(0xff));
-        // but are stripped when they are redundant
-        if (flags & COND_CANON_INTS) != 0 {
-            assert_eq!(
-                seconds_tester(&[0, 0, 0, 0xff], *flags).unwrap_err().1,
-                ErrorCode::AssertSecondsAbsolute
-            );
-            assert_eq!(
-                seconds_tester(&[0, 0, 0, 0x80], *flags).unwrap_err().1,
-                ErrorCode::AssertSecondsAbsolute
-            );
-            assert_eq!(
-                seconds_tester(&[0, 0, 0, 0x7f], *flags).unwrap_err().1,
-                ErrorCode::AssertSecondsAbsolute
-            );
-            assert_eq!(
-                seconds_tester(&[0, 0, 0], *flags).unwrap_err().1,
-                ErrorCode::AssertSecondsAbsolute
-            );
-            assert_eq!(
-                seconds_tester(&[0], *flags).unwrap_err().1,
-                ErrorCode::AssertSecondsAbsolute
-            );
-        } else {
-            assert_eq!(seconds_tester(&[0, 0, 0, 0xff], *flags), Ok(0xff));
-            assert_eq!(seconds_tester(&[0, 0, 0, 0x80], *flags), Ok(0x80));
-            assert_eq!(seconds_tester(&[0, 0, 0, 0x7f], *flags), Ok(0x7f));
-            assert_eq!(seconds_tester(&[0, 0, 0], *flags), Ok(0));
-            assert_eq!(seconds_tester(&[0], *flags), Ok(0));
-        }
+    // leading zeros are somtimes necessary to make values positive
+    assert_eq!(seconds_tester(&[0, 0xff]), Ok(0xff));
+    // but are stripped when they are redundant
+    assert_eq!(
+        seconds_tester(&[0, 0, 0, 0xff]).unwrap_err().1,
+        ErrorCode::AssertSecondsAbsolute
+    );
+    assert_eq!(
+        seconds_tester(&[0, 0, 0, 0x80]).unwrap_err().1,
+        ErrorCode::AssertSecondsAbsolute
+    );
+    assert_eq!(
+        seconds_tester(&[0, 0, 0, 0x7f]).unwrap_err().1,
+        ErrorCode::AssertSecondsAbsolute
+    );
+    assert_eq!(
+        seconds_tester(&[0, 0, 0]).unwrap_err().1,
+        ErrorCode::AssertSecondsAbsolute
+    );
+    assert_eq!(
+        seconds_tester(&[0]).unwrap_err().1,
+        ErrorCode::AssertSecondsAbsolute
+    );
 
-        // seconds aren't allowed to be > 2^64 (i.e. 9 bytes)
-        assert_eq!(
-            seconds_tester(
-                &[0x7f, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff],
-                *flags
-            )
+    // seconds aren't allowed to be > 2^64 (i.e. 9 bytes)
+    assert_eq!(
+        seconds_tester(&[0x7f, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff],)
             .unwrap_err()
             .1,
-            ErrorCode::AssertSecondsAbsolute
-        );
+        ErrorCode::AssertSecondsAbsolute
+    );
 
-        // this is small enough though
-        assert_eq!(
-            seconds_tester(&[0, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff], *flags),
-            Ok(0xffffffffffffffff)
-        );
+    // this is small enough though
+    assert_eq!(
+        seconds_tester(&[0, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff]),
+        Ok(0xffffffffffffffff)
+    );
 
-        let mut a = Allocator::new();
-        let pair = a.new_pair(a.null(), a.null()).unwrap();
-        let mut range_cache = RangeSet::new();
-        assert_eq!(
-            parse_seconds(
-                &mut a,
-                pair,
-                ErrorCode::AssertSecondsAbsolute,
-                &mut range_cache,
-                *flags
-            ),
-            Err(ValidationErr(pair, ErrorCode::AssertSecondsAbsolute))
-        );
-    }
+    let mut a = Allocator::new();
+    let pair = a.new_pair(a.null(), a.null()).unwrap();
+    assert_eq!(
+        parse_seconds(&mut a, pair, ErrorCode::AssertSecondsAbsolute),
+        Err(ValidationErr(pair, ErrorCode::AssertSecondsAbsolute))
+    );
 }
--- wheel/src/api.rs
+++ wheel/src/api.rs
@@ -1,7 +1,6 @@
 use crate::coin::Coin;
 use crate::run_generator::{PySpend, PySpendBundleConditions, __pyo3_get_function_run_generator};
 use chia::gen::flags::COND_ARGS_NIL;
-use chia::gen::flags::COND_CANON_INTS;
 use chia::gen::flags::NO_UNKNOWN_CONDS;
 use chia::gen::flags::STRICT_ARGS_COUNT;
 use chia::merkle_set::compute_merkle_set_root as compute_merkle_root_impl;
@@ -18,12 +17,8 @@ use crate::run_program::{
     __pyo3_get_function_run_chia_program, __pyo3_get_function_serialized_length,
 };
 
-pub const MEMPOOL_MODE: u32 = NO_NEG_DIV
-    | COND_CANON_INTS
-    | NO_UNKNOWN_CONDS
-    | NO_UNKNOWN_OPS
-    | COND_ARGS_NIL
-    | STRICT_ARGS_COUNT;
+pub const MEMPOOL_MODE: u32 =
+    NO_NEG_DIV | NO_UNKNOWN_CONDS | NO_UNKNOWN_OPS | COND_ARGS_NIL | STRICT_ARGS_COUNT;
 
 #[pyfunction]
 pub fn compute_merkle_set_root<'p>(
@@ -42,7 +37,6 @@ pub fn chia_rs(_py: Python, m: &PyModule) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(run_generator, m)?)?;
     m.add_class::<PySpendBundleConditions>()?;
     m.add_class::<PySpend>()?;
-    m.add("COND_CANON_INTS", COND_CANON_INTS)?;
     m.add("COND_ARGS_NIL", COND_ARGS_NIL)?;
     m.add("NO_UNKNOWN_CONDS", NO_UNKNOWN_CONDS)?;
     m.add("STRICT_ARGS_COUNT", STRICT_ARGS_COUNT)?;
