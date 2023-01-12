9a195721e17449d249f57ee252fcf29d130340c7
arvidn <arvid@libtorrent.org>
2023-01-12 15:51:44 +0100

    improve error handling a bit

--- wheel/src/adapt_response.rs
+++ wheel/src/adapt_response.rs
@@ -8,11 +8,12 @@ use pyo3::types::PyDict;
 
 pub fn eval_err_to_pyresult<T>(py: Python, eval_err: EvalErr, allocator: Allocator) -> PyResult<T> {
     let node = Node::new(&allocator, eval_err.0);
-    let blob = node_to_bytes(&node).unwrap();
-    let msg = eval_err.1;
     let ctx: &PyDict = PyDict::new(py);
+    let msg = eval_err.1;
     ctx.set_item("msg", msg)?;
-    ctx.set_item("blob", blob)?;
+    if let Ok(blob) = node_to_bytes(&node) {
+        ctx.set_item("blob", blob)?;
+    }
     Err(py
         .run("raise ValueError(msg, bytes(blob).hex())", None, Some(ctx))
         .unwrap_err())
