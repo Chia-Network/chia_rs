6d12cdcddf3bb12a42e991391e6adfc4615f1cda
Rigidity <me@rigidnetwork.com>
2023-07-11 13:08:05 -0400

    Reformat chia-protocol fuzz

--- chia-protocol/fuzz/fuzz_targets/parse-tx-info.rs
+++ chia-protocol/fuzz/fuzz_targets/parse-tx-info.rs
@@ -1,8 +1,8 @@
 #![no_main]
+use chia_protocol::Streamable;
+use chia_protocol::TransactionsInfo;
 use libfuzzer_sys::fuzz_target;
 use std::io::Cursor;
-use chia_protocol::TransactionsInfo;
-use chia_protocol::Streamable;
 
 fuzz_target!(|data: &[u8]| {
     let _ret = <TransactionsInfo as Streamable>::parse(&mut Cursor::<&[u8]>::new(data));
