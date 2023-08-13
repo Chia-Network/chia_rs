ce2d5f3fc3035e431eb82ddcfd1ca03f57f41565
Rigidity <me@rigidnetwork.com>
2023-08-13 01:06:22 -0400

    Add support for deriving unnamed structs

--- clvm-derive/src/from_clvm.rs
+++ clvm-derive/src/from_clvm.rs
@@ -1,6 +1,6 @@
-use proc_macro2::TokenStream;
+use proc_macro2::{Ident, TokenStream};
 use quote::quote;
-use syn::{parse_quote, Data, DataStruct, DeriveInput, Fields};
+use syn::{parse_quote, spanned::Spanned, Data, DeriveInput, Fields, Type};
 
 use crate::helpers::{add_trait_bounds, parse_args, Repr};
 
@@ -8,17 +8,34 @@ pub fn from_clvm(mut ast: DeriveInput) -> TokenStream {
     let args = parse_args(&ast.attrs);
     let crate_name = quote!(clvm_traits);
 
-    let fields = match &ast.data {
-        Data::Struct(DataStruct {
-            fields: Fields::Named(fields),
-            ..
-        }) => &fields.named,
-        _ => panic!("expected a struct with named fields"),
+    let field_types: Vec<Type>;
+    let field_names: Vec<Ident>;
+
+    match &ast.data {
+        Data::Struct(data_struct) => match &data_struct.fields {
+            Fields::Named(fields) => {
+                let fields = &fields.named;
+                field_types = fields.iter().map(|field| field.ty.clone()).collect();
+                field_names = fields
+                    .iter()
+                    .map(|field| field.ident.clone().unwrap())
+                    .collect();
+            }
+            Fields::Unnamed(fields) => {
+                let fields = &fields.unnamed;
+                field_types = fields.iter().map(|field| field.ty.clone()).collect();
+                field_names = fields
+                    .iter()
+                    .enumerate()
+                    .map(|(i, field)| Ident::new(&format!("field_{i}"), field.span()))
+                    .collect();
+            }
+            Fields::Unit => panic!("unit structs are not supported"),
+        },
+        _ => panic!("expected struct with named or unnamed fields"),
     };
 
     let struct_name = &ast.ident;
-    let field_type = fields.iter().map(|field| &field.ty);
-    let field_names = fields.iter().map(|field| &field.ident);
     let destructure_names = field_names.clone();
 
     // `match_macro` decodes a nested tuple containing each of the struct field types within.
@@ -45,7 +62,7 @@ pub fn from_clvm(mut ast: DeriveInput) -> TokenStream {
         #[automatically_derived]
         impl #impl_generics #crate_name::FromClvm for #struct_name #ty_generics #where_clause {
             fn from_clvm(a: &clvmr::Allocator, node: clvmr::allocator::NodePtr) -> #crate_name::Result<Self> {
-                let #destructure_macro!( #( #destructure_names, )* ) = <#match_macro!( #( #field_type ),* ) as #crate_name::FromClvm>::from_clvm(a, node)?;
+                let #destructure_macro!( #( #destructure_names, )* ) = <#match_macro!( #( #field_types ),* ) as #crate_name::FromClvm>::from_clvm(a, node)?;
                 Ok(Self { #( #field_names, )* })
             }
         }
