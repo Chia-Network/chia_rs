d4e26373b41cc1c1bf4e9a84cbbbd5572b8ffbef
Rigidity <me@rigidnetwork.com>
2023-08-14 12:25:42 -0400

    Reorganize bytes tests

--- chia-protocol/src/bytes.rs
+++ chia-protocol/src/bytes.rs
@@ -400,237 +400,238 @@ impl<'py> FromPyObject<'py> for Bytes {
 }
 
 #[cfg(test)]
-use rstest::rstest;
+mod tests {
+    use super::*;
 
-#[cfg(test)]
-#[rstest]
-// Bytess32
-#[case(
-    "0000000000000000000000000000000000000000000000000000000000000000",
-    "0000000000000000000000000000000000000000000000000000000000000000",
-    true
-)]
-#[case(
-    "0000000000000000000000000000000000000000000000000000000000000000",
-    "0000000000000000000000000000000000000000000000000000000000000100",
-    false
-)]
-#[case(
-    "fff0000000000000000000000000000000000000000000000000000000000100",
-    "fff0000000000000000000000000000000000000000000000000000000000100",
-    true
-)]
-// Bytes
-#[case("000000", "000000", true)]
-#[case("123456", "125456", false)]
-#[case("000001", "00000001", false)]
-#[case("00000001", "000001", false)]
-#[case("ffff01", "ffff01", true)]
-#[case("", "", true)]
-fn test_bytes_comparisons(#[case] lhs: &str, #[case] rhs: &str, #[case] expect_equal: bool) {
-    let lhs_vec: Vec<u8> = hex::decode(lhs).expect("hex::decode");
-    let rhs_vec: Vec<u8> = hex::decode(rhs).expect("hex::decode");
-    let lhs_slice = &lhs_vec[..];
-    let rhs_slice = &rhs_vec[..];
-    if lhs_vec.len() == 32 && rhs_vec.len() == 32 {
-        let lhs = Bytes32::from(&lhs_vec);
-        let rhs = Bytes32::from(&rhs_vec);
+    use rstest::rstest;
 
-        assert_eq!(lhs.len(), 32);
-        assert_eq!(rhs.len(), 32);
+    #[rstest]
+    // Bytess32
+    #[case(
+        "0000000000000000000000000000000000000000000000000000000000000000",
+        "0000000000000000000000000000000000000000000000000000000000000000",
+        true
+    )]
+    #[case(
+        "0000000000000000000000000000000000000000000000000000000000000000",
+        "0000000000000000000000000000000000000000000000000000000000000100",
+        false
+    )]
+    #[case(
+        "fff0000000000000000000000000000000000000000000000000000000000100",
+        "fff0000000000000000000000000000000000000000000000000000000000100",
+        true
+    )]
+    // Bytes
+    #[case("000000", "000000", true)]
+    #[case("123456", "125456", false)]
+    #[case("000001", "00000001", false)]
+    #[case("00000001", "000001", false)]
+    #[case("ffff01", "ffff01", true)]
+    #[case("", "", true)]
+    fn test_bytes_comparisons(#[case] lhs: &str, #[case] rhs: &str, #[case] expect_equal: bool) {
+        let lhs_vec: Vec<u8> = hex::decode(lhs).expect("hex::decode");
+        let rhs_vec: Vec<u8> = hex::decode(rhs).expect("hex::decode");
+        let lhs_slice = &lhs_vec[..];
+        let rhs_slice = &rhs_vec[..];
+        if lhs_vec.len() == 32 && rhs_vec.len() == 32 {
+            let lhs = Bytes32::from(&lhs_vec);
+            let rhs = Bytes32::from(&rhs_vec);
 
-        assert_eq!(lhs.is_empty(), lhs_vec.is_empty());
-        assert_eq!(rhs.is_empty(), rhs_vec.is_empty());
+            assert_eq!(lhs.len(), 32);
+            assert_eq!(rhs.len(), 32);
 
-        // Bytes32 compare against arrays of the same size, not slices
-        let lhs_array: &[u8; 32] = lhs_slice.try_into().unwrap();
-        let rhs_array: &[u8; 32] = rhs_slice.try_into().unwrap();
-        if expect_equal {
-            assert_eq!(lhs, rhs);
-            assert_eq!(rhs, lhs);
+            assert_eq!(lhs.is_empty(), lhs_vec.is_empty());
+            assert_eq!(rhs.is_empty(), rhs_vec.is_empty());
+
+            // Bytes32 compare against arrays of the same size, not slices
+            let lhs_array: &[u8; 32] = lhs_slice.try_into().unwrap();
+            let rhs_array: &[u8; 32] = rhs_slice.try_into().unwrap();
+            if expect_equal {
+                assert_eq!(lhs, rhs);
+                assert_eq!(rhs, lhs);
+
+                // array comparisons
+                assert_eq!(lhs, rhs_array);
+                assert_eq!(rhs, lhs_array);
+                assert_eq!(lhs_array, rhs);
+                assert_eq!(rhs_array, lhs);
+
+                assert_eq!(lhs, *rhs_array);
+                assert_eq!(rhs, *lhs_array);
+                assert_eq!(*lhs_array, rhs);
+                assert_eq!(*rhs_array, lhs);
+
+                // slice comparisona
+                assert_eq!(lhs, rhs_slice);
+                assert_eq!(rhs, lhs_slice);
+                assert_eq!(lhs_slice, rhs);
+                assert_eq!(rhs_slice, lhs);
+            } else {
+                assert!(lhs != rhs);
+                assert!(rhs != lhs);
+
+                // array comparisons
+                assert!(lhs != rhs_array);
+                assert!(rhs != lhs_array);
+                assert!(lhs_array != rhs);
+                assert!(rhs_array != lhs);
+
+                assert!(lhs != *rhs_array);
+                assert!(rhs != *lhs_array);
+                assert!(*lhs_array != rhs);
+                assert!(*rhs_array != lhs);
+
+                // slice comparisons
+                assert!(lhs != rhs_slice);
+                assert!(rhs != lhs_slice);
+                assert!(lhs_slice != rhs);
+                assert!(rhs_slice != lhs);
+            }
+        } else {
+            let lhs = Bytes::from(lhs_vec.clone());
+            let rhs = Bytes::from(rhs_vec.clone());
+
+            assert_eq!(lhs.len(), lhs_vec.len());
+            assert_eq!(rhs.len(), rhs_vec.len());
+
+            assert_eq!(lhs.is_empty(), lhs_vec.is_empty());
+            assert_eq!(rhs.is_empty(), rhs_vec.is_empty());
 
             // array comparisons
-            assert_eq!(lhs, rhs_array);
-            assert_eq!(rhs, lhs_array);
-            assert_eq!(lhs_array, rhs);
-            assert_eq!(rhs_array, lhs);
+            let array: &[u8; 3] = &[1, 2, 3];
+            assert!(lhs != array);
+            assert!(rhs != array);
+            assert!(array != lhs);
+            assert!(array != rhs);
+            assert!(lhs != *array);
+            assert!(rhs != *array);
+            assert!(*array != lhs);
+            assert!(*array != rhs);
 
-            assert_eq!(lhs, *rhs_array);
-            assert_eq!(rhs, *lhs_array);
-            assert_eq!(*lhs_array, rhs);
-            assert_eq!(*rhs_array, lhs);
+            if expect_equal {
+                assert_eq!(lhs, rhs);
+                assert_eq!(rhs, lhs);
 
-            // slice comparisona
-            assert_eq!(lhs, rhs_slice);
-            assert_eq!(rhs, lhs_slice);
-            assert_eq!(lhs_slice, rhs);
-            assert_eq!(rhs_slice, lhs);
-        } else {
-            assert!(lhs != rhs);
-            assert!(rhs != lhs);
+                // slice comparisons
+                assert_eq!(lhs, rhs_slice);
+                assert_eq!(rhs, lhs_slice);
+                assert_eq!(lhs_slice, rhs);
+                assert_eq!(rhs_slice, lhs);
 
-            // array comparisons
-            assert!(lhs != rhs_array);
-            assert!(rhs != lhs_array);
-            assert!(lhs_array != rhs);
-            assert!(rhs_array != lhs);
+                // vec comparisons
+                assert_eq!(lhs, rhs_vec);
+                assert_eq!(rhs, lhs_vec);
+                assert_eq!(lhs_vec, rhs);
+                assert_eq!(rhs_vec, lhs);
+            } else {
+                assert!(lhs != rhs);
+                assert!(rhs != lhs);
 
-            assert!(lhs != *rhs_array);
-            assert!(rhs != *lhs_array);
-            assert!(*lhs_array != rhs);
-            assert!(*rhs_array != lhs);
+                // slice comparisons
+                assert!(lhs != rhs_slice);
+                assert!(rhs != lhs_slice);
+                assert!(lhs_slice != rhs);
+                assert!(rhs_slice != lhs);
 
-            // slice comparisons
-            assert!(lhs != rhs_slice);
-            assert!(rhs != lhs_slice);
-            assert!(lhs_slice != rhs);
-            assert!(rhs_slice != lhs);
-        }
-    } else {
-        let lhs = Bytes::from(lhs_vec.clone());
-        let rhs = Bytes::from(rhs_vec.clone());
-
-        assert_eq!(lhs.len(), lhs_vec.len());
-        assert_eq!(rhs.len(), rhs_vec.len());
-
-        assert_eq!(lhs.is_empty(), lhs_vec.is_empty());
-        assert_eq!(rhs.is_empty(), rhs_vec.is_empty());
-
-        // array comparisons
-        let array: &[u8; 3] = &[1, 2, 3];
-        assert!(lhs != array);
-        assert!(rhs != array);
-        assert!(array != lhs);
-        assert!(array != rhs);
-        assert!(lhs != *array);
-        assert!(rhs != *array);
-        assert!(*array != lhs);
-        assert!(*array != rhs);
-
-        if expect_equal {
-            assert_eq!(lhs, rhs);
-            assert_eq!(rhs, lhs);
-
-            // slice comparisons
-            assert_eq!(lhs, rhs_slice);
-            assert_eq!(rhs, lhs_slice);
-            assert_eq!(lhs_slice, rhs);
-            assert_eq!(rhs_slice, lhs);
-
-            // vec comparisons
-            assert_eq!(lhs, rhs_vec);
-            assert_eq!(rhs, lhs_vec);
-            assert_eq!(lhs_vec, rhs);
-            assert_eq!(rhs_vec, lhs);
-        } else {
-            assert!(lhs != rhs);
-            assert!(rhs != lhs);
-
-            // slice comparisons
-            assert!(lhs != rhs_slice);
-            assert!(rhs != lhs_slice);
-            assert!(lhs_slice != rhs);
-            assert!(rhs_slice != lhs);
-
-            // vec comparisons
-            assert!(lhs != rhs_vec);
-            assert!(rhs != lhs_vec);
-            assert!(lhs_vec != rhs);
-            assert!(rhs_vec != lhs);
+                // vec comparisons
+                assert!(lhs != rhs_vec);
+                assert!(rhs != lhs_vec);
+                assert!(lhs_vec != rhs);
+                assert!(rhs_vec != lhs);
+            }
         }
     }
-}
 
-#[cfg(test)]
-fn from_bytes<T: Streamable + std::fmt::Debug + std::cmp::PartialEq>(buf: &[u8], expected: T) {
-    let mut input = Cursor::<&[u8]>::new(buf);
-    assert_eq!(T::parse(&mut input).unwrap(), expected);
-}
+    fn from_bytes<T: Streamable + std::fmt::Debug + std::cmp::PartialEq>(buf: &[u8], expected: T) {
+        let mut input = Cursor::<&[u8]>::new(buf);
+        assert_eq!(T::parse(&mut input).unwrap(), expected);
+    }
 
-#[cfg(test)]
-fn from_bytes_fail<T: Streamable + std::fmt::Debug + std::cmp::PartialEq>(
-    buf: &[u8],
-    expected: chia_error::Error,
-) {
-    let mut input = Cursor::<&[u8]>::new(buf);
-    assert_eq!(T::parse(&mut input).unwrap_err(), expected);
-}
+    fn from_bytes_fail<T: Streamable + std::fmt::Debug + std::cmp::PartialEq>(
+        buf: &[u8],
+        expected: chia_error::Error,
+    ) {
+        let mut input = Cursor::<&[u8]>::new(buf);
+        assert_eq!(T::parse(&mut input).unwrap_err(), expected);
+    }
 
-#[cfg(test)]
-fn stream<T: Streamable>(v: &T) -> Vec<u8> {
-    let mut buf = Vec::<u8>::new();
-    v.stream(&mut buf).unwrap();
-    let mut ctx1 = Sha256::new();
-    let mut ctx2 = Sha256::new();
-    v.update_digest(&mut ctx1);
-    ctx2.update(&buf);
-    assert_eq!(&ctx1.finalize(), &ctx2.finalize());
-    buf
-}
+    fn stream<T: Streamable>(v: &T) -> Vec<u8> {
+        let mut buf = Vec::<u8>::new();
+        v.stream(&mut buf).unwrap();
+        let mut ctx1 = Sha256::new();
+        let mut ctx2 = Sha256::new();
+        v.update_digest(&mut ctx1);
+        ctx2.update(&buf);
+        assert_eq!(&ctx1.finalize(), &ctx2.finalize());
+        buf
+    }
 
-#[test]
-fn test_stream_bytes32() {
-    let buf: &[u8] = &[
-        1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25,
-        26, 27, 28, 29, 30, 31, 32,
-    ];
-    let out = stream(&Bytes32::from(buf));
-    assert_eq!(&buf, &out);
-}
+    #[test]
+    fn test_stream_bytes32() {
+        let buf: &[u8] = &[
+            1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24,
+            25, 26, 27, 28, 29, 30, 31, 32,
+        ];
+        let out = stream(&Bytes32::from(buf));
+        assert_eq!(&buf, &out);
+    }
 
-#[test]
-fn test_stream_bytes() {
-    let val: Bytes = vec![
-        1_u8, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24,
-        25, 26, 27, 28, 29, 30, 31, 32,
-    ]
-    .into();
-    println!("{:?}", val);
-    let buf = stream(&val);
-    println!("buf: {:?}", buf);
-    from_bytes(&buf, val);
-}
+    #[test]
+    fn test_stream_bytes() {
+        let val: Bytes = vec![
+            1_u8, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23,
+            24, 25, 26, 27, 28, 29, 30, 31, 32,
+        ]
+        .into();
+        println!("{:?}", val);
+        let buf = stream(&val);
+        println!("buf: {:?}", buf);
+        from_bytes(&buf, val);
+    }
 
-#[test]
-fn test_parse_bytes_empty() {
-    let buf: &[u8] = &[0, 0, 0, 0];
-    from_bytes::<Bytes>(buf, [].to_vec().into());
-}
+    #[test]
+    fn test_parse_bytes_empty() {
+        let buf: &[u8] = &[0, 0, 0, 0];
+        from_bytes::<Bytes>(buf, [].to_vec().into());
+    }
 
-#[test]
-fn test_parse_bytes() {
-    let buf: &[u8] = &[0, 0, 0, 3, 1, 2, 3];
-    from_bytes::<Bytes>(buf, [1_u8, 2, 3].to_vec().into());
-}
+    #[test]
+    fn test_parse_bytes() {
+        let buf: &[u8] = &[0, 0, 0, 3, 1, 2, 3];
+        from_bytes::<Bytes>(buf, [1_u8, 2, 3].to_vec().into());
+    }
 
-#[test]
-fn test_parse_truncated_len() {
-    let buf: &[u8] = &[0, 0, 1];
-    from_bytes_fail::<Bytes>(buf, chia_error::Error::EndOfBuffer);
-}
+    #[test]
+    fn test_parse_truncated_len() {
+        let buf: &[u8] = &[0, 0, 1];
+        from_bytes_fail::<Bytes>(buf, chia_error::Error::EndOfBuffer);
+    }
 
-#[test]
-fn test_parse_truncated() {
-    let buf: &[u8] = &[0, 0, 0, 4, 1, 2, 3];
-    from_bytes_fail::<Bytes>(buf, chia_error::Error::EndOfBuffer);
-}
+    #[test]
+    fn test_parse_truncated() {
+        let buf: &[u8] = &[0, 0, 0, 4, 1, 2, 3];
+        from_bytes_fail::<Bytes>(buf, chia_error::Error::EndOfBuffer);
+    }
 
-#[test]
-fn test_parse_bytes32() {
-    let buf: &[u8] = &[
-        1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25,
-        26, 27, 28, 29, 30, 31, 32,
-    ];
-    from_bytes::<Bytes32>(buf, Bytes32::from(buf));
-    from_bytes_fail::<Bytes32>(&buf[0..30], chia_error::Error::EndOfBuffer);
-}
+    #[test]
+    fn test_parse_bytes32() {
+        let buf: &[u8] = &[
+            1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24,
+            25, 26, 27, 28, 29, 30, 31, 32,
+        ];
+        from_bytes::<Bytes32>(buf, Bytes32::from(buf));
+        from_bytes_fail::<Bytes32>(&buf[0..30], chia_error::Error::EndOfBuffer);
+    }
 
-#[test]
-fn test_parse_bytes48() {
-    let buf: &[u8] = &[
-        1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25,
-        26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48,
-    ];
-    from_bytes::<Bytes48>(buf, Bytes48::from(buf));
-    from_bytes_fail::<Bytes48>(&buf[0..47], chia_error::Error::EndOfBuffer);
+    #[test]
+    fn test_parse_bytes48() {
+        let buf: &[u8] = &[
+            1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24,
+            25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46,
+            47, 48,
+        ];
+        from_bytes::<Bytes48>(buf, Bytes48::from(buf));
+        from_bytes_fail::<Bytes48>(&buf[0..47], chia_error::Error::EndOfBuffer);
+    }
 }
