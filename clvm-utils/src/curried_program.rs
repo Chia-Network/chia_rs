50623a8eb6f4ba41e597998d892fc2237f02b101
Rigidity <me@rigidnetwork.com>
2023-08-21 15:40:24 -0400

    Make CurriedProgram named

--- clvm-utils/src/curried_program.rs
+++ clvm-utils/src/curried_program.rs
@@ -5,7 +5,10 @@ use clvm_traits::{
 use clvmr::{allocator::NodePtr, Allocator};
 
 #[derive(Debug, Clone)]
-pub struct CurriedProgram<T>(pub NodePtr, pub T);
+pub struct CurriedProgram<T> {
+    pub program: NodePtr,
+    pub args: T,
+}
 
 impl<T> FromClvm for CurriedProgram<T>
 where
@@ -15,7 +18,7 @@ where
         let destructure_list!(_, destructure_quote!(program), args) =
             <match_list!(MatchByte<2>, match_quote!(NodePtr), T)>::from_clvm(a, ptr)?;
 
-        Ok(Self(program, args))
+        Ok(Self { program, args })
     }
 }
 
@@ -24,7 +27,7 @@ where
     T: ToClvm,
 {
     fn to_clvm(&self, a: &mut Allocator) -> Result<NodePtr> {
-        clvm_list!(2, clvm_quote!(self.0), self.1.to_clvm(a)?).to_clvm(a)
+        clvm_list!(2, clvm_quote!(self.program), self.args.to_clvm(a)?).to_clvm(a)
     }
 }
 
@@ -44,16 +47,19 @@ mod tests {
     {
         let a = &mut Allocator::new();
 
-        let curry = CurriedProgram(program.to_clvm(a).unwrap(), args.clone())
-            .to_clvm(a)
-            .unwrap();
+        let curry = CurriedProgram {
+            program: program.to_clvm(a).unwrap(),
+            args: args.clone(),
+        }
+        .to_clvm(a)
+        .unwrap();
         let actual = node_to_bytes(a, curry).unwrap();
         assert_eq!(hex::encode(actual), expected);
 
         let curried = CurriedProgram::<A>::from_clvm(a, curry).unwrap();
-        let round_program = T::from_clvm(a, curried.0).unwrap();
+        let round_program = T::from_clvm(a, curried.program).unwrap();
         assert_eq!(round_program, program);
-        assert_eq!(curried.1, args);
+        assert_eq!(curried.args, args);
     }
 
     #[test]
