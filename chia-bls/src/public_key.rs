b9eb6aa5a55c7390a6bf9412a5e2cc36ce5ee099
arvidn <arvid@libtorrent.org>
2023-08-29 14:28:19 +0200

    organize public_key tests in its own module

--- chia-bls/src/public_key.rs
+++ chia-bls/src/public_key.rs
@@ -299,223 +299,218 @@ impl ToClvm for PublicKey {
 }
 
 #[cfg(test)]
-use hex::FromHex;
+mod tests {
+    use super::*;
+    use crate::SecretKey;
+    use hex::FromHex;
+    use rand::rngs::StdRng;
+    use rand::{Rng, SeedableRng};
+    use rstest::rstest;
 
-#[cfg(test)]
-use crate::SecretKey;
+    #[test]
+    fn test_derive_unhardened() {
+        let sk_hex = "52d75c4707e39595b27314547f9723e5530c01198af3fc5849d9a7af65631efb";
+        let sk = SecretKey::from_bytes(&<[u8; 32]>::from_hex(sk_hex).unwrap()).unwrap();
+        let pk = sk.public_key();
 
-#[test]
-fn test_derive_unhardened() {
-    let sk_hex = "52d75c4707e39595b27314547f9723e5530c01198af3fc5849d9a7af65631efb";
-    let sk = SecretKey::from_bytes(&<[u8; 32]>::from_hex(sk_hex).unwrap()).unwrap();
-    let pk = sk.public_key();
-
-    // make sure deriving the secret keys produce the same public keys as
-    // deriving the public key
-    for idx in 0..4_usize {
-        let derived_sk = sk.derive_unhardened(idx as u32);
-        let derived_pk = pk.derive_unhardened(idx as u32);
-        assert_eq!(derived_pk.to_bytes(), derived_sk.public_key().to_bytes());
+        // make sure deriving the secret keys produce the same public keys as
+        // deriving the public key
+        for idx in 0..4_usize {
+            let derived_sk = sk.derive_unhardened(idx as u32);
+            let derived_pk = pk.derive_unhardened(idx as u32);
+            assert_eq!(derived_pk.to_bytes(), derived_sk.public_key().to_bytes());
+        }
     }
-}
 
-#[test]
-#[cfg(feature = "py-bindings")]
-fn test_generator() {
-    assert_eq!(
-        hex::encode(&PublicKey::generator().to_bytes()),
-        "97f1d3a73197d7942695638c4fa9ac0fc3688c4f9774b905a14e3a3f171bac586c55e83ff97a1aeffb3af00adb22c6bb"
-    );
-}
-
-#[cfg(test)]
-use rand::{Rng, SeedableRng};
-
-#[cfg(test)]
-use rand::rngs::StdRng;
-
-#[cfg(test)]
-use rstest::rstest;
-
-#[test]
-fn test_from_bytes() {
-    let mut rng = StdRng::seed_from_u64(1337);
-    let mut data = [0u8; 48];
-    for _i in 0..50 {
-        rng.fill(data.as_mut_slice());
-        // clear the bits that mean infinity
-        data[0] = 0x80;
-        // just any random bytes are not a valid key and should fail
-        match PublicKey::from_bytes(&data) {
-            Err(Error::InvalidPublicKey(err)) => {
-                assert!([
-                    BLST_ERROR::BLST_BAD_ENCODING,
-                    BLST_ERROR::BLST_POINT_NOT_ON_CURVE
-                ]
-                .contains(&err));
-            }
-            Err(e) => {
-                panic!("unexpected error from_bytes(): {e}");
-            }
-            Ok(v) => {
-                panic!("unexpected value from_bytes(): {v:?}");
+    #[test]
+    fn test_from_bytes() {
+        let mut rng = StdRng::seed_from_u64(1337);
+        let mut data = [0u8; 48];
+        for _i in 0..50 {
+            rng.fill(data.as_mut_slice());
+            // clear the bits that mean infinity
+            data[0] = 0x80;
+            // just any random bytes are not a valid key and should fail
+            match PublicKey::from_bytes(&data) {
+                Err(Error::InvalidPublicKey(err)) => {
+                    assert!([
+                        BLST_ERROR::BLST_BAD_ENCODING,
+                        BLST_ERROR::BLST_POINT_NOT_ON_CURVE
+                    ]
+                    .contains(&err));
+                }
+                Err(e) => {
+                    panic!("unexpected error from_bytes(): {e}");
+                }
+                Ok(v) => {
+                    panic!("unexpected value from_bytes(): {v:?}");
+                }
             }
         }
     }
-}
 
-#[cfg(test)]
-#[rstest]
-#[case("c00000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000001", Error::G1NotCanonical)]
-#[case("c08000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000", Error::G1NotCanonical)]
-#[case("c80000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000", Error::G1NotCanonical)]
-#[case("e00000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000", Error::G1NotCanonical)]
-#[case("d00000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000", Error::G1NotCanonical)]
-#[case("800000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000", Error::G1InfinityNotZero)]
-#[case("400000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000", Error::G1InfinityInvalidBits)]
-fn test_from_bytes_failures(#[case] input: &str, #[case()] error: Error) {
-    let bytes: [u8; 48] = hex::decode(input).unwrap().try_into().unwrap();
-    assert_eq!(PublicKey::from_bytes(&bytes).unwrap_err(), error);
-}
+    #[rstest]
+    #[case("c00000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000001", Error::G1NotCanonical)]
+    #[case("c08000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000", Error::G1NotCanonical)]
+    #[case("c80000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000", Error::G1NotCanonical)]
+    #[case("e00000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000", Error::G1NotCanonical)]
+    #[case("d00000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000", Error::G1NotCanonical)]
+    #[case("800000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000", Error::G1InfinityNotZero)]
+    #[case("400000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000", Error::G1InfinityInvalidBits)]
+    fn test_from_bytes_failures(#[case] input: &str, #[case()] error: Error) {
+        let bytes: [u8; 48] = hex::decode(input).unwrap().try_into().unwrap();
+        assert_eq!(PublicKey::from_bytes(&bytes).unwrap_err(), error);
+    }
 
-#[test]
-fn test_from_bytes_infinity() {
-    let bytes: [u8; 48] = hex::decode("c00000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000").unwrap().try_into().unwrap();
-    let pk = PublicKey::from_bytes(&bytes).unwrap();
-    assert_eq!(pk, PublicKey::default());
-}
+    #[test]
+    fn test_from_bytes_infinity() {
+        let bytes: [u8; 48] = hex::decode("c00000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000").unwrap().try_into().unwrap();
+        let pk = PublicKey::from_bytes(&bytes).unwrap();
+        assert_eq!(pk, PublicKey::default());
+    }
 
-#[test]
-fn test_get_fingerprint() {
-    let bytes: [u8; 48] = hex::decode("997cc43ed8788f841fcf3071f6f212b89ba494b6ebaf1bda88c3f9de9d968a61f3b7284a5ee13889399ca71a026549a2")
+    #[test]
+    fn test_get_fingerprint() {
+        let bytes: [u8; 48] = hex::decode("997cc43ed8788f841fcf3071f6f212b89ba494b6ebaf1bda88c3f9de9d968a61f3b7284a5ee13889399ca71a026549a2")
         .unwrap()
         .as_slice()
         .try_into()
         .unwrap();
-    let pk = PublicKey::from_bytes(&bytes).unwrap();
-    assert_eq!(pk.get_fingerprint(), 651010559);
-}
-
-#[test]
-fn test_aggregate_pubkey() {
-    // from blspy import PrivateKey
-    // from blspy import AugSchemeMPL
-    // sk = PrivateKey.from_bytes(bytes.fromhex("52d75c4707e39595b27314547f9723e5530c01198af3fc5849d9a7af65631efb"))
-    // pk = sk.get_g1()
-    // pk + pk
-    // <G1Element b1b8033286299e7f238aede0d3fea48d133a1e233139085f72c102c2e6cc1f8a4ea64ed2838c10bbd2ef8f78ef271bf3>
-    // pk + pk + pk
-    // <G1Element a8bc2047d90c04a12e8c38050ec0feb4417b4d5689165cd2cea8a7903aad1778e36548a46d427b5ec571364515e456d6>
-
-    let sk_hex = "52d75c4707e39595b27314547f9723e5530c01198af3fc5849d9a7af65631efb";
-    let sk = SecretKey::from_bytes(&<[u8; 32]>::from_hex(sk_hex).unwrap()).unwrap();
-    let pk = sk.public_key();
-    let pk2 = &pk + &pk;
-    let pk3 = &pk + &pk + &pk;
-
-    assert_eq!(pk2, PublicKey::from_bytes(&<[u8; 48]>::from_hex("b1b8033286299e7f238aede0d3fea48d133a1e233139085f72c102c2e6cc1f8a4ea64ed2838c10bbd2ef8f78ef271bf3").unwrap()).unwrap());
-    assert_eq!(pk3, PublicKey::from_bytes(&<[u8; 48]>::from_hex("a8bc2047d90c04a12e8c38050ec0feb4417b4d5689165cd2cea8a7903aad1778e36548a46d427b5ec571364515e456d6").unwrap()).unwrap());
-}
-
-#[test]
-fn test_roundtrip() {
-    let mut rng = StdRng::seed_from_u64(1337);
-    let mut data = [0u8; 32];
-    for _i in 0..50 {
-        rng.fill(data.as_mut_slice());
-        let sk = SecretKey::from_seed(&data);
-        let pk = sk.public_key();
-        let bytes = pk.to_bytes();
-        let pk2 = PublicKey::from_bytes(&bytes).unwrap();
-        assert_eq!(pk, pk2);
+        let pk = PublicKey::from_bytes(&bytes).unwrap();
+        assert_eq!(pk.get_fingerprint(), 651010559);
     }
-}
 
-#[test]
-fn test_default_is_valid() {
-    let pk = PublicKey::default();
-    assert!(pk.is_valid());
-}
+    #[test]
+    fn test_aggregate_pubkey() {
+        // from blspy import PrivateKey
+        // from blspy import AugSchemeMPL
+        // sk = PrivateKey.from_bytes(bytes.fromhex("52d75c4707e39595b27314547f9723e5530c01198af3fc5849d9a7af65631efb"))
+        // pk = sk.get_g1()
+        // pk + pk
+        // <G1Element b1b8033286299e7f238aede0d3fea48d133a1e233139085f72c102c2e6cc1f8a4ea64ed2838c10bbd2ef8f78ef271bf3>
+        // pk + pk + pk
+        // <G1Element a8bc2047d90c04a12e8c38050ec0feb4417b4d5689165cd2cea8a7903aad1778e36548a46d427b5ec571364515e456d6>
 
-#[test]
-fn test_infinity_is_valid() {
-    let mut data = [0u8; 48];
-    data[0] = 0xc0;
-    let pk = PublicKey::from_bytes(&data).unwrap();
-    assert!(pk.is_valid());
-}
-
-#[test]
-fn test_is_valid() {
-    let mut rng = StdRng::seed_from_u64(1337);
-    let mut data = [0u8; 32];
-    for _i in 0..50 {
-        rng.fill(data.as_mut_slice());
-        let sk = SecretKey::from_seed(&data);
+        let sk_hex = "52d75c4707e39595b27314547f9723e5530c01198af3fc5849d9a7af65631efb";
+        let sk = SecretKey::from_bytes(&<[u8; 32]>::from_hex(sk_hex).unwrap()).unwrap();
         let pk = sk.public_key();
+        let pk2 = &pk + &pk;
+        let pk3 = &pk + &pk + &pk;
+
+        assert_eq!(pk2, PublicKey::from_bytes(&<[u8; 48]>::from_hex("b1b8033286299e7f238aede0d3fea48d133a1e233139085f72c102c2e6cc1f8a4ea64ed2838c10bbd2ef8f78ef271bf3").unwrap()).unwrap());
+        assert_eq!(pk3, PublicKey::from_bytes(&<[u8; 48]>::from_hex("a8bc2047d90c04a12e8c38050ec0feb4417b4d5689165cd2cea8a7903aad1778e36548a46d427b5ec571364515e456d6").unwrap()).unwrap());
+    }
+
+    #[test]
+    fn test_roundtrip() {
+        let mut rng = StdRng::seed_from_u64(1337);
+        let mut data = [0u8; 32];
+        for _i in 0..50 {
+            rng.fill(data.as_mut_slice());
+            let sk = SecretKey::from_seed(&data);
+            let pk = sk.public_key();
+            let bytes = pk.to_bytes();
+            let pk2 = PublicKey::from_bytes(&bytes).unwrap();
+            assert_eq!(pk, pk2);
+        }
+    }
+
+    #[test]
+    fn test_default_is_valid() {
+        let pk = PublicKey::default();
         assert!(pk.is_valid());
     }
-}
 
-#[test]
-fn test_hash() {
-    fn hash<T: std::hash::Hash>(v: T) -> u64 {
-        use std::collections::hash_map::DefaultHasher;
-        let mut h = DefaultHasher::new();
-        v.hash(&mut h);
-        h.finish()
+    #[test]
+    fn test_infinity_is_valid() {
+        let mut data = [0u8; 48];
+        data[0] = 0xc0;
+        let pk = PublicKey::from_bytes(&data).unwrap();
+        assert!(pk.is_valid());
     }
 
-    let mut rng = StdRng::seed_from_u64(1337);
-    let mut data = [0u8; 32];
-    rng.fill(data.as_mut_slice());
+    #[test]
+    fn test_is_valid() {
+        let mut rng = StdRng::seed_from_u64(1337);
+        let mut data = [0u8; 32];
+        for _i in 0..50 {
+            rng.fill(data.as_mut_slice());
+            let sk = SecretKey::from_seed(&data);
+            let pk = sk.public_key();
+            assert!(pk.is_valid());
+        }
+    }
 
-    let sk = SecretKey::from_seed(&data);
-    let pk1 = sk.public_key();
-    let pk2 = pk1.derive_unhardened(1);
-    let pk3 = pk1.derive_unhardened(2);
+    #[test]
+    fn test_hash() {
+        fn hash<T: std::hash::Hash>(v: T) -> u64 {
+            use std::collections::hash_map::DefaultHasher;
+            let mut h = DefaultHasher::new();
+            v.hash(&mut h);
+            h.finish()
+        }
 
-    assert!(hash(pk2) != hash(pk3));
-    assert!(hash(pk1.derive_unhardened(42)) == hash(pk1.derive_unhardened(42)));
-}
+        let mut rng = StdRng::seed_from_u64(1337);
+        let mut data = [0u8; 32];
+        rng.fill(data.as_mut_slice());
 
-#[test]
-fn test_debug() {
-    let mut data = [0u8; 48];
-    data[0] = 0xc0;
-    let pk = PublicKey::from_bytes(&data).unwrap();
-    assert_eq!(format!("{:?}", pk), hex::encode(data));
-}
+        let sk = SecretKey::from_seed(&data);
+        let pk1 = sk.public_key();
+        let pk2 = pk1.derive_unhardened(1);
+        let pk3 = pk1.derive_unhardened(2);
 
-#[test]
-fn test_to_from_clvm() {
-    let mut a = Allocator::new();
-    let bytes = hex::decode("997cc43ed8788f841fcf3071f6f212b89ba494b6ebaf1bda88c3f9de9d968a61f3b7284a5ee13889399ca71a026549a2").expect("hex::decode()");
-    let ptr = a.new_atom(&bytes).expect("new_atom");
+        assert!(hash(pk2) != hash(pk3));
+        assert!(hash(pk1.derive_unhardened(42)) == hash(pk1.derive_unhardened(42)));
+    }
 
-    let pk = PublicKey::from_clvm(&a, ptr).expect("from_clvm");
-    assert_eq!(pk.to_bytes(), &bytes[..]);
+    #[test]
+    fn test_debug() {
+        let mut data = [0u8; 48];
+        data[0] = 0xc0;
+        let pk = PublicKey::from_bytes(&data).unwrap();
+        assert_eq!(format!("{:?}", pk), hex::encode(data));
+    }
 
-    let pk_ptr = pk.to_clvm(&mut a).expect("to_clvm");
-    assert!(a.atom_eq(pk_ptr, ptr));
-}
+    #[test]
+    fn test_to_from_clvm() {
+        let mut a = Allocator::new();
+        let bytes = hex::decode("997cc43ed8788f841fcf3071f6f212b89ba494b6ebaf1bda88c3f9de9d968a61f3b7284a5ee13889399ca71a026549a2").expect("hex::decode()");
+        let ptr = a.new_atom(&bytes).expect("new_atom");
 
-#[test]
-fn test_from_clvm_failure() {
-    let mut a = Allocator::new();
-    let ptr = a.new_pair(a.one(), a.one()).expect("new_pair");
-    assert_eq!(
-        PublicKey::from_clvm(&a, ptr).unwrap_err(),
-        clvm_traits::Error::ExpectedAtom(ptr)
-    );
+        let pk = PublicKey::from_clvm(&a, ptr).expect("from_clvm");
+        assert_eq!(pk.to_bytes(), &bytes[..]);
+
+        let pk_ptr = pk.to_clvm(&mut a).expect("to_clvm");
+        assert!(a.atom_eq(pk_ptr, ptr));
+    }
+
+    #[test]
+    fn test_from_clvm_failure() {
+        let mut a = Allocator::new();
+        let ptr = a.new_pair(a.one(), a.one()).expect("new_pair");
+        assert_eq!(
+            PublicKey::from_clvm(&a, ptr).unwrap_err(),
+            clvm_traits::Error::ExpectedAtom(ptr)
+        );
+    }
 }
 
 #[cfg(test)]
 #[cfg(feature = "py-bindings")]
 mod pytests {
-
     use super::*;
+    use crate::SecretKey;
+    use rand::rngs::StdRng;
+    use rand::{Rng, SeedableRng};
     use rstest::rstest;
+
+    #[test]
+    fn test_generator() {
+        assert_eq!(
+            hex::encode(&PublicKey::generator().to_bytes()),
+            "97f1d3a73197d7942695638c4fa9ac0fc3688c4f9774b905a14e3a3f171bac586c55e83ff97a1aeffb3af00adb22c6bb"
+        );
+    }
 
     #[test]
     fn test_json_dict_roundtrip() {
