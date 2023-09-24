20f2e0686249f0f9c118d774b1507dba6824fc2b
arvidn <arvid@libtorrent.org>
2023-09-24 12:54:03 +0200

    the generator tests are expensive and slow in debug builds. Only run them in release builds

--- src/gen/mod.rs
+++ src/gen/mod.rs
@@ -10,5 +10,8 @@ pub mod sanitize_int;
 pub mod solution_generator;
 pub mod validation_error;
 
+// these tests are large and expensive. They take a long time to run in
+// unoptimized builds. Only run these with --release
+#[cfg(not(debug_assertions))]
 #[cfg(test)]
 mod test_generators;
