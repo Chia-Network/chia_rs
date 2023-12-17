556b4e17164eb802bb726e75ec6272824bf2ec30
arvidn <arvid@libtorrent.org>
2023-12-17 13:11:43 +0100

    fix argument conversion for Program::_run() to match SerializedProgram._run()

--- tests/test_program_fidelity.py
+++ tests/test_program_fidelity.py
@@ -3,6 +3,7 @@ from typing import List, Tuple, Optional
 import string
 import chia_rs
 from chia.types.blockchain_format.program import Program as ChiaProgram
+from chia.types.blockchain_format.serialized_program import SerializedProgram
 from random import Random
 
 def rand_bytes(rnd: Random) -> bytes:
@@ -41,17 +42,35 @@ def rand_object(rnd: Random) -> object:
     types = [rand_optional, rand_int, rand_string, rand_bytes, rand_program, rand_list, rand_rust_program]
     return rnd.sample(types, 1)[0](rnd)
 
+def recursive_replace(o: object) -> object:
+    if isinstance(o, list):
+        ret = []
+        for i in o:
+            ret.append(recursive_replace(i))
+        return ret
+    elif isinstance(o, chia_rs.Program):
+        return SerializedProgram.from_bytes(o.to_bytes())
+    else:
+        return o
+
 def test_run_program() -> None:
 
     rust_identity = chia_rs.Program.from_bytes(b"\x01")
-    py_identity = ChiaProgram.from_bytes(b"\x01")
+    py_identity = SerializedProgram.from_bytes(b"\x01")
 
     rnd = Random()
     for _ in range(10000):
         args = rand_object(rnd)
 
-        py_ret = py_identity._run(10000, 0, args)
+        # the python SerializedProgram treats itself specially, the rust
+        # Program treats itself specially, but they don't recognize each other,
+        # so they will produce different results in this regard
         rust_ret = rust_identity._run(10000, 0, args)
+
+        # Replace rust Program with the python SerializedProgram.
+        args = recursive_replace(args)
+
+        py_ret = py_identity._run(10000, 0, args)
 
         assert rust_ret == py_ret
 
