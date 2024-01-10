f3369fdeab21f3b4a684db4fc4d9b2acd4193c38
Rigidity <me@rigidnetwork.com>
2024-01-10 10:15:43 -0500

    Add Default to empty streamables

--- chia-protocol/src/message_struct.rs
+++ chia-protocol/src/message_struct.rs
@@ -1,45 +1,56 @@
 #[macro_export]
 macro_rules! message_struct {
-    ($name:ident {$($field:ident: $t:ty $(,)? )*}) => {
-        #[cfg_attr(feature = "py-bindings", pyo3::pyclass(get_all, frozen), derive(chia_py_streamable_macro::PyJsonDict, chia_py_streamable_macro::PyStreamable))]
-        #[derive(Streamable, Hash, Debug, Clone, Eq, PartialEq)]
-        #[cfg_attr(fuzzing, derive(arbitrary::Arbitrary))]
-        pub struct $name {
-            $(pub $field: $t),*
-        }
-
-        #[cfg(not(feature = "py-bindings"))]
-        #[allow(clippy::too_many_arguments)]
-        impl $name {
-            pub fn new ( $($field: $t),* ) -> $name {
-                $name { $($field),* }
-            }
-        }
+    ( $name:ident $( { $( $field:ident: $t:ty ),* $(,)? } )? ) => {
+        $crate::streamable_struct!( $name $( { $( $field: $t, )* } )? );
 
         impl ChiaProtocolMessage for $name {
             fn msg_type() -> $crate::ProtocolMessageTypes {
                 $crate::ProtocolMessageTypes::$name
             }
         }
-    }
+    };
 }
 
 #[macro_export]
 macro_rules! streamable_struct {
-    ($name:ident {$($field:ident: $t:ty $(,)? )*}) => {
+    ( $name:ident { $( $field:ident: $t:ty ),+ $(,)? } ) => {
+        $crate::streamable_struct!( impl $name { $( $field: $t, )* } );
+    };
+
+    ( $name:ident {} ) => {
+        $crate::streamable_struct!( impl $name {} );
+
+        impl Default for $name {
+            fn default() -> Self {
+                Self {}
+            }
+        }
+    };
+
+    ( $name:ident ) => {
+        $crate::streamable_struct!( impl $name );
+
+        impl Default for $name {
+            fn default() -> Self {
+                Self
+            }
+        }
+    };
+
+    ( impl $name:ident $( { $( $field:ident: $t:ty ),* $(,)? } )? ) => {
         #[cfg_attr(feature = "py-bindings", pyo3::pyclass(get_all, frozen), derive(chia_py_streamable_macro::PyJsonDict, chia_py_streamable_macro::PyStreamable))]
         #[derive(Streamable, Hash, Debug, Clone, Eq, PartialEq)]
         #[cfg_attr(fuzzing, derive(arbitrary::Arbitrary))]
-        pub struct $name {
-            $(pub $field: $t),*
-        }
+        pub struct $name $( {
+            $( pub $field: $t ),*
+        } )?
 
         #[cfg(not(feature = "py-bindings"))]
         #[allow(clippy::too_many_arguments)]
         impl $name {
-            pub fn new ( $($field: $t),* ) -> $name {
-                $name { $($field),* }
+            pub fn new ( $( $( $field: $t),* )? ) -> $name {
+                Self $( { $($field),* } )?
             }
         }
-    }
+    };
 }
