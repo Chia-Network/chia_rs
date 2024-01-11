75ab14cf1e04087a767dae55cf87adf527307619
Rigidity <me@rigidnetwork.com>
2024-01-10 22:31:43 -0500

    Derive ToClvm, FromClvm, Arbitrary for Proof

--- chia-wallet/src/proof.rs
+++ chia-wallet/src/proof.rs
@@ -1,49 +1,16 @@
-use arbitrary::{Arbitrary, Unstructured};
 use chia_protocol::Bytes32;
-use clvm_traits::{ClvmDecoder, ClvmEncoder, FromClvm, FromClvmError, ToClvm, ToClvmError};
+use clvm_traits::{FromClvm, ToClvm};
 
-#[derive(Debug, Clone, PartialEq, Eq)]
+#[derive(Debug, Clone, PartialEq, Eq, ToClvm, FromClvm)]
+#[cfg_attr(fuzzing, derive(arbitrary::Arbitrary))]
+#[clvm(untagged, tuple)]
 pub enum Proof {
     Lineage(LineageProof),
     Eve(EveProof),
 }
 
-impl<N> FromClvm<N> for Proof {
-    fn from_clvm(decoder: &impl ClvmDecoder<Node = N>, node: N) -> Result<Self, FromClvmError> {
-        LineageProof::from_clvm(decoder, decoder.clone_node(&node))
-            .map(Self::Lineage)
-            .or_else(|_| EveProof::from_clvm(decoder, node).map(Self::Eve))
-    }
-}
-
-impl<N> ToClvm<N> for Proof {
-    fn to_clvm(&self, encoder: &mut impl ClvmEncoder<Node = N>) -> Result<N, ToClvmError> {
-        match self {
-            Self::Lineage(lineage_proof) => lineage_proof.to_clvm(encoder),
-            Self::Eve(eve_proof) => eve_proof.to_clvm(encoder),
-        }
-    }
-}
-
-impl<'a> Arbitrary<'a> for Proof {
-    fn arbitrary(u: &mut Unstructured<'a>) -> arbitrary::Result<Self> {
-        let is_eve = u.ratio(3, 10)?;
-        if is_eve {
-            Ok(Self::Eve(EveProof {
-                parent_coin_info: (u.arbitrary::<[u8; 32]>()?).into(),
-                amount: u.arbitrary()?,
-            }))
-        } else {
-            Ok(Self::Lineage(LineageProof {
-                parent_coin_info: (u.arbitrary::<[u8; 32]>()?).into(),
-                inner_puzzle_hash: (u.arbitrary::<[u8; 32]>()?).into(),
-                amount: u.arbitrary()?,
-            }))
-        }
-    }
-}
-
 #[derive(Debug, Clone, PartialEq, Eq, ToClvm, FromClvm)]
+#[cfg_attr(fuzzing, derive(arbitrary::Arbitrary))]
 #[clvm(list)]
 pub struct LineageProof {
     pub parent_coin_info: Bytes32,
@@ -52,6 +19,7 @@ pub struct LineageProof {
 }
 
 #[derive(Debug, Clone, PartialEq, Eq, ToClvm, FromClvm)]
+#[cfg_attr(fuzzing, derive(arbitrary::Arbitrary))]
 #[clvm(list)]
 pub struct EveProof {
     pub parent_coin_info: Bytes32,
