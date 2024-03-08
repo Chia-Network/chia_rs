5d8231e402b93cb50761735258fd99e1a0aa66a6
arvidn <arvid@libtorrent.org>
2024-03-08 18:33:26 +0100

    improve fuzzer coverage

--- crates/chia-consensus/fuzz/fuzz_targets/parse-spend.rs
+++ crates/chia-consensus/fuzz/fuzz_targets/parse-spend.rs
@@ -3,11 +3,11 @@ use libfuzzer_sys::fuzz_target;
 
 use chia_consensus::gen::get_puzzle_and_solution::parse_coin_spend;
 use clvmr::allocator::Allocator;
-use fuzzing_utils::{make_tree, BitCursor};
+use fuzzing_utils::{make_list, BitCursor};
 
 fuzz_target!(|data: &[u8]| {
     let mut a = Allocator::new();
-    let input = make_tree(&mut a, &mut BitCursor::new(data), false);
+    let input = make_list(&mut a, &mut BitCursor::new(data));
 
     let _ret = parse_coin_spend(&a, input);
 });
