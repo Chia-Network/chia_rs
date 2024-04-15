d8f641aa79f6f5bd5bf0cb746ceed2f6829256f6
arvidn <arvid@libtorrent.org>
2024-04-15 19:18:28 +0200

    simplify merkle_set test

--- tests/test_merkle_set.py
+++ tests/test_merkle_set.py
@@ -1,4 +1,4 @@
-from typing import List, Optional, Any, Callable
+from typing import List, Optional, Any, Callable, Protocol, Tuple
 
 import sys
 import time
@@ -14,6 +14,20 @@ from chia.util.merkle_set import (
 from chia.types.blockchain_format.sized_bytes import bytes32
 
 
+def check_proof(
+    proof: bytes,
+    deserialize: Callable[[bytes], Any],
+    *,
+    root: bytes32,
+    item: bytes32,
+) -> None:
+    proof_tree = deserialize(proof)
+    assert proof_tree.get_root() == root
+    included, proof2 = proof_tree.is_included_already_hashed(item)
+    assert included
+    assert proof == proof2
+
+
 def check_tree(leafs: List[bytes32]) -> None:
     ru_tree = RustMerkleSet(leafs)
     py_tree = PythonMerkleSet()
@@ -21,6 +35,7 @@ def check_tree(leafs: List[bytes32]) -> None:
         py_tree.add_already_hashed(item)
 
     assert py_tree.get_root() == ru_tree.get_root()
+    root = bytes32(ru_tree.get_root())
 
     for item in leafs:
         py_included, py_proof = py_tree.is_included_already_hashed(item)
@@ -33,24 +48,10 @@ def check_tree(leafs: List[bytes32]) -> None:
             print(f"ru: {ru_proof.hex()}")
         assert py_proof == ru_proof
 
-        py_proof_tree = py_deserialize_proof(py_proof)
-        ru_proof_tree = ru_deserialize_proof(ru_proof)
-        py_proof_tree2 = py_deserialize_proof(ru_proof)
-        ru_proof_tree2 = ru_deserialize_proof(py_proof)
-
-        py_included, py_proof2 = py_proof_tree.is_included_already_hashed(item)
-        assert py_included
-        ru_included, ru_proof2 = ru_proof_tree.is_included_already_hashed(item)
-        assert ru_included
-        py_included, py_proof3 = py_proof_tree2.is_included_already_hashed(item)
-        assert py_included
-        ru_included, ru_proof3 = ru_proof_tree2.is_included_already_hashed(item)
-        assert ru_included
-
-        assert py_proof2 == py_proof
-        assert ru_proof2 == ru_proof
-        assert py_proof3 == ru_proof
-        assert ru_proof3 == py_proof
+        check_proof(py_proof, py_deserialize_proof, root=root, item=item)
+        check_proof(ru_proof, py_deserialize_proof, root=root, item=item)
+        check_proof(py_proof, ru_deserialize_proof, root=root, item=item)
+        check_proof(ru_proof, ru_deserialize_proof, root=root, item=item)
 
 
 def h(b: str) -> bytes32:
