26de3701a6743f840f62f7c2c8d2bfba82e43a05
Rigidity <me@rigidnetwork.com>
2024-04-23 16:26:18 -0400

    Add impls for ToClvm and FromClvm to bool

--- crates/clvm-traits/src/from_clvm.rs
+++ crates/clvm-traits/src/from_clvm.rs
@@ -55,6 +55,19 @@ clvm_primitive!(i128);
 clvm_primitive!(usize);
 clvm_primitive!(isize);
 
+impl<N> FromClvm<N> for bool {
+    fn from_clvm(decoder: &impl ClvmDecoder<Node = N>, node: N) -> Result<Self, FromClvmError> {
+        let atom = decoder.decode_atom(&node)?;
+        match atom.as_ref() {
+            [] => Ok(false),
+            [1] => Ok(true),
+            _ => Err(FromClvmError::Custom(
+                "expected boolean value of either `()` or `1`".to_string(),
+            )),
+        }
+    }
+}
+
 impl<N, A, B> FromClvm<N> for (A, B)
 where
     A: FromClvm<N>,
@@ -209,6 +222,19 @@ mod tests {
         assert_eq!(decode(a, "81e5"), Ok(-27i32));
         assert_eq!(decode(a, "80"), Ok(-0));
         assert_eq!(decode(a, "8180"), Ok(-128i8));
+    }
+
+    #[test]
+    fn test_bool() {
+        let a = &mut Allocator::new();
+        assert_eq!(decode(a, "80"), Ok(false));
+        assert_eq!(decode(a, "01"), Ok(true));
+        assert_eq!(
+            decode::<bool>(a, "05"),
+            Err(FromClvmError::Custom(
+                "expected boolean value of either `()` or `1`".to_string(),
+            ))
+        )
     }
 
     #[test]
