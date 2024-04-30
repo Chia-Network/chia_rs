3c8fa304209398f517f316513cec302dfee133d0
Matthew Howard <m.o.howard97@gmail.com>
2024-04-30 17:32:56 +0100

    add get_name_puzzle_conditions

--- crates/chia-consensus/src/npc_result.rs
+++ crates/chia-consensus/src/npc_result.rs
@@ -1,7 +1,11 @@
 use chia_consensus::gen::conditions::{
     parse_conditions, MempoolVisitor, ParseState, Spend, SpendBundleConditions,
 };
-
+use crate::BlockGenerator;
+use crate::ConsensusConstants;
+use crate::gen::{run_block_generator, run_block_generator2};
+use crate::get_flags_for_height_and_constants;
+use crate::gen::flags::{MEMPOOL_MODE, ENABLE_MESSAGE_CONDITIONS};
 
 #[cfg_attr(
     feature = "py-bindings",
@@ -14,4 +18,25 @@ use chia_consensus::gen::conditions::{
 pub struct NPCResult {
     error: Option<u16>,
     conds: Option<SpendBundleConditions>,
+}
+
+pub fn get_name_puzzle_conditions(
+    generator: BlockGenerator,
+    max_cost: i64,
+    mempool_mode: bool,
+    height: u32,
+    constants: ConsensusConstants,
+) -> NPCResult {
+    let run_block = if height >= constants.HARD_FORK_FIX_HEIGHT {run_block_generator2} else {run_block_generator};
+    let mut flags = get_flags_for_height_and_constants(height, constants);
+    if mempool_mode {flags = flags | MEMPOOL_MODE};
+    let mut block_args = Vec<&[u8]>::new();
+    for gen in generator.generator_refs {
+        block_args.push(gen.to_bytes());
+    }
+    result = run_block(generator.program.as_bytes(), block_args, max_cost, flags);
+    match result {
+        Err(val_err) => NPCResult(val_err, None),
+        Some(val_res) => NPCResult(None, val_res),
+    }
 }
