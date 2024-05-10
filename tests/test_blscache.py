94c8f7b3581eca5f76c577b1ca00e9f5f56cfef7
Matthew Howard <m.o.howard97@gmail.com>
2024-05-10 15:32:46 +0100

    black tests

--- tests/test_blscache.py
+++ tests/test_blscache.py
@@ -4,6 +4,7 @@ from chia.util.hash import std_hash
 from chia.util.lru_cache import LRUCache
 from chia.util import cached_bls as cached_bls_old
 
+
 def test_instantiation() -> None:
     bls_cache = BLSCache()
     assert bls_cache.len() == 0
@@ -55,6 +56,7 @@ def test_cache_limit() -> None:
     assert result
     assert bls_cache.len() == 3
 
+
 # old Python tests ported
 # benchmark old vs new BLSCache
 def test_cached_bls():
@@ -79,7 +81,9 @@ def test_cached_bls():
 
     # Verify with empty cache and populate it
     assert cached_bls.aggregate_verify(pks_half, msgs_half, agg_sig_half)
-    assert cached_bls_old.aggregate_verify(pks_half_bytes, msgs_half, agg_sig_half, True)
+    assert cached_bls_old.aggregate_verify(
+        pks_half_bytes, msgs_half, agg_sig_half, True
+    )
 
     # Verify with partial cache hit
     assert cached_bls.aggregate_verify(pks, msgs, agg_sig)
@@ -95,21 +99,28 @@ def test_cached_bls():
     # Verify signatures and cache pairings one at a time
     for pk, msg, sig in zip(pks_half, msgs_half, sigs_half):
         assert cached_bls.aggregate_verify([pk], [msg], sig)
-        assert cached_bls_old.aggregate_verify([bytes(pk)], [msg], sig, True, local_cache)
-    
+        assert cached_bls_old.aggregate_verify(
+            [bytes(pk)], [msg], sig, True, local_cache
+        )
+
     # Verify the same messages with aggregated signature (full cache hit)
     assert cached_bls.aggregate_verify(pks_half, msgs_half, agg_sig_half)
-    assert cached_bls_old.aggregate_verify(pks_half_bytes, msgs_half, agg_sig_half, False, local_cache)
+    assert cached_bls_old.aggregate_verify(
+        pks_half_bytes, msgs_half, agg_sig_half, False, local_cache
+    )
 
     # Verify more messages (partial cache hit)
     assert cached_bls.aggregate_verify(pks, msgs, agg_sig)
     assert cached_bls_old.aggregate_verify(pks_bytes, msgs, agg_sig, False, local_cache)
 
+
 def test_cached_bls_repeat_pk():
     cached_bls = BLSCache()
     n_keys = 400
     seed = b"a" * 32
-    sks = [AugSchemeMPL.key_gen(seed) for i in range(n_keys)] + [AugSchemeMPL.key_gen(std_hash(seed))]
+    sks = [AugSchemeMPL.key_gen(seed) for i in range(n_keys)] + [
+        AugSchemeMPL.key_gen(std_hash(seed))
+    ]
     pks = [sk.get_g1() for sk in sks]
     pks_bytes = [bytes(sk.get_g1()) for sk in sks]
 
