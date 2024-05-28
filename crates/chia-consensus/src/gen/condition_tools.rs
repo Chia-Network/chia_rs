1fe83b9888d332b008b87e929157e841f134a371
Matthew Howard <m.o.howard97@gmail.com>
2024-05-28 19:32:38 +0100

    fix make_aggsig_final_message

--- crates/chia-consensus/src/gen/condition_tools.rs
+++ crates/chia-consensus/src/gen/condition_tools.rs
@@ -2,6 +2,7 @@ use std::collections::HashMap;
 use crate::gen::owned_conditions::{OwnedSpendBundleConditions, OwnedSpend};
 use chia_bls::PublicKey;
 use chia_protocol::Coin;
+use chia_protocol::Bytes;
 use crate::gen::validation_error::ErrorCode;
 use crate::gen::opcodes::{AGG_SIG_AMOUNT, AGG_SIG_PARENT, AGG_SIG_PARENT_PUZZLE,
     AGG_SIG_PUZZLE_AMOUNT, AGG_SIG_ME, AGG_SIG_UNSAFE, AGG_SIG_PUZZLE, AGG_SIG_PARENT_AMOUNT, ConditionOpcode};
@@ -46,32 +47,27 @@ fn make_aggsig_final_message(
     spend: OwnedSpend,
     agg_sig_additional_data: HashMap<ConditionOpcode, Vec<u8>>,
 ) -> Vec<u8> {
-    let coin: Coin = Coin::new(
-        spend.parent_id.clone(),
-        spend.puzzle_hash.clone(),
-        spend.coin_amount as u64,
-    );
-
-    let addendum = match opcode {
-        AGG_SIG_PARENT => coin.parent_coin_info.clone(),
-        AGG_SIG_PUZZLE => coin.puzzle_hash.clone(),
-        AGG_SIG_AMOUNT => int_to_bytes(coin.amount),
+    let addendum: Vec<u8> = match opcode {
+        AGG_SIG_PARENT => spend.parent_id.as_slice().to_vec(),
+        AGG_SIG_PUZZLE => spend.puzzle_hash.as_slice().to_vec(),
+        AGG_SIG_AMOUNT => u64_to_bytes(spend.coin_amount).as_slice().to_vec(),
         AGG_SIG_PUZZLE_AMOUNT => {
-            let mut data = coin.puzzle_hash.clone();
-            data.extend(int_to_bytes(coin.amount));
-            data
+            [spend.parent_id.as_slice(), u64_to_bytes(spend.coin_amount).as_slice()].concat()
         }
         AGG_SIG_PARENT_AMOUNT => {
-            let mut data = coin.parent_coin_info.clone();
-            data.extend(int_to_bytes(coin.amount));
-            data
+            [spend.parent_id.as_slice(), u64_to_bytes(spend.coin_amount).as_slice()].concat()
         }
         AGG_SIG_PARENT_PUZZLE => {
-            let mut data = coin.parent_coin_info.clone();
-            data.extend(coin.puzzle_hash.clone());
-            data
+            [spend.parent_id.as_slice(), spend.puzzle_hash.as_slice()].concat()
         }
-        AGG_SIG_ME => coin_name(&coin),
+        AGG_SIG_ME => {
+            let coin: Coin = Coin::new(
+                spend.parent_id.clone(),
+                spend.puzzle_hash.clone(),
+                spend.coin_amount as u64,
+            );
+            coin.coin_id().as_slice().to_vec()
+        },
     };
 
     let mut result = msg.to_vec();
@@ -80,7 +76,30 @@ fn make_aggsig_final_message(
         result.extend(additional_data.clone());
     }
 
-    result.as_slice()
+    result
+}
+
+fn u64_to_bytes(val: u64) -> Bytes {
+    let amount_bytes: [u8; 8] = val.to_be_bytes();
+        if val >= 0x8000000000000000_u64 {
+            let mut ret = Vec::<u8>::new();
+            ret.push(0_u8);
+            ret.extend(amount_bytes);
+            Bytes::new(ret)
+        } else {
+            let start = match val {
+                n if n >= 0x80000000000000_u64 => 0,
+                n if n >= 0x800000000000_u64 => 1,
+                n if n >= 0x8000000000_u64 => 2,
+                n if n >= 0x80000000_u64 => 3,
+                n if n >= 0x800000_u64 => 4,
+                n if n >= 0x8000_u64 => 5,
+                n if n >= 0x80_u64 => 6,
+                n if n > 0 => 7,
+                _ => 8,
+            };
+            Bytes::new(amount_bytes[start..].to_vec())
+        }
 }
 
 fn agg_sig_additional_data(agg_sig_data: &[u8]) -> HashMap<ConditionOpcode, Vec<u8>> {
