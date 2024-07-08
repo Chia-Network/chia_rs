24135fd1190221a6ff2066a300ccc64a72500c60
arvidn <arvid@libtorrent.org>
2024-07-08 17:35:12 +0200

    add test for printing ValueError from CLVM

--- tests/test_program.py
+++ tests/test_program.py
@@ -23,3 +23,14 @@ def test_raise_program() -> None:
         assert False
     except ValueError as e:
         assert f"{e}" == "('clvm raise', '86666f6f626172')"
+
+
+def test_repr() -> None:
+    temp = Program.to([8, (1, "foo")])
+    assert f"{temp}" == "Program(ff08ffff0183666f6f80)"
+
+    try:
+        run_chia_program(bytes(temp), bytes.fromhex("00"), 1100000000, 0)
+        assert False
+    except ValueError as e:
+        assert f"{e}" == "('clvm raise', '83666f6f')"
