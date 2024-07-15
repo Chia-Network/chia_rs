3280753d1db54e2f36118d54f8fd010951d87839
Matthew Howard <m.o.howard97@gmail.com>
2024-07-15 16:15:40 +0100

    create pybinding for get_name_puzzle_conditions

--- wheel/src/api.rs
+++ wheel/src/api.rs
@@ -15,6 +15,7 @@ use chia_consensus::merkle_set::compute_merkle_set_root as compute_merkle_root_i
 use chia_consensus::merkle_tree::{validate_merkle_proof, MerkleSet};
 use chia_consensus::multiprocess_validation::validate_clvm_and_signature;
 
+use chia_consensus::npc_result::get_name_puzzle_conditions;
 use chia_protocol::{
     BlockRecord, Bytes32, ChallengeBlockInfo, ChallengeChainSubSlot, ClassgroupElement, Coin,
     CoinSpend, CoinState, CoinStateFilters, CoinStateUpdate, EndOfSubSlotBundle, Foliage,
@@ -396,6 +397,22 @@ pub fn py_validate_clvm_and_signature(
     Ok((owned_conditions, additions, duration.as_secs_f32()))
 }
 
+#[pyfunction]
+#[pyo3(name = "get_name_puzzle_conditions")]
+pub fn py_get_name_puzzle_conditions(
+    spend_bundle: &SpendBundle,
+    max_cost: u64,
+    constants: &ConsensusConstants,
+    mempool_mode: bool,
+    height: u32,
+) -> PyResult<OwnedSpendBundleConditions> {
+    let osbc = get_name_puzzle_conditions(spend_bundle, max_cost, mempool_mode, height, constants).map_err(|e| {
+        let error_code: u32 = e.1.into();
+        PyErr::new::<PyTypeError, _>(error_code) 
+    })?;
+    Ok(osbc)
+}
+
 #[pymodule]
 pub fn chia_rs(_py: Python<'_>, m: &Bound<'_, PyModule>) -> PyResult<()> {
     // generator functions
@@ -427,6 +444,7 @@ pub fn chia_rs(_py: Python<'_>, m: &Bound<'_, PyModule>) -> PyResult<()> {
 
     // multithread validattion
     m.add_function(wrap_pyfunction!(py_validate_clvm_and_signature, m)?)?;
+    m.add_function(wrap_pyfunction!(py_get_name_puzzle_conditions, m)?)?;
 
     // clvm functions
     m.add("COND_ARGS_NIL", COND_ARGS_NIL)?;
