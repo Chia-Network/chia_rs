1270f1da021d5a10e707a5fadaabc76dd96cb752
Matthew Howard <m.o.howard97@gmail.com>
2024-07-31 13:08:38 +0100

    initial commit

---
+++ crates/chia-consensus/src/spendbundle_conditions.rs
@@ -0,0 +1,69 @@
+use crate::allocator::make_allocator;
+use crate::consensus_constants::ConsensusConstants;
+use crate::gen::conditions::{
+    process_single_spend, validate_conditions, MempoolVisitor, ParseState, SpendBundleConditions,
+};
+use crate::gen::flags::MEMPOOL_MODE;
+use crate::gen::owned_conditions::OwnedSpendBundleConditions;
+use crate::gen::run_block_generator::subtract_cost;
+use crate::gen::validation_error::ValidationErr;
+use crate::spendbundle_validation::get_flags_for_height_and_constants;
+use chia_protocol::SpendBundle;
+use clvm_utils::{tree_hash_cached, TreeHash};
+use clvmr::allocator::{Allocator, NodePtr};
+use clvmr::chia_dialect::ChiaDialect;
+use clvmr::chia_dialect::LIMIT_HEAP;
+use clvmr::reduction::Reduction;
+use clvmr::run_program::run_program;
+use clvmr::serde::node_from_bytes;
+use std::collections::{HashMap, HashSet};
+
+pub fn get_conditions_from_spendbundle(
+    spend_bundle: &SpendBundle,
+    max_cost: u64,
+    height: u32,
+    constants: &ConsensusConstants,
+) -> Result<OwnedSpendBundleConditions, ValidationErr> {
+    let flags = get_flags_for_height_and_constants(height, constants) | MEMPOOL_MODE;
+
+    // below is an adapted version of the code from run_block_generators::run_block_generator2()
+    // it assumes no block references are passed in
+    let mut cost_left = max_cost;
+    let dialect = ChiaDialect::new(flags);
+    let mut a: Allocator = make_allocator(LIMIT_HEAP);
+    let mut ret = SpendBundleConditions::default();
+    let mut state = ParseState::default();
+    let mut cache = HashMap::<NodePtr, TreeHash>::new();
+
+    for coin_spend in &spend_bundle.coin_spends {
+        // process the spend
+        let puz = node_from_bytes(&mut a, coin_spend.puzzle_reveal.as_slice())?;
+        let sol = node_from_bytes(&mut a, coin_spend.solution.as_slice())?;
+        let parent = a.new_atom(coin_spend.coin.parent_coin_info.as_slice())?;
+        let amount = a.new_number(coin_spend.coin.amount.into())?;
+        let Reduction(clvm_cost, conditions) = run_program(&mut a, &dialect, puz, sol, cost_left)?;
+
+        subtract_cost(&a, &mut cost_left, clvm_cost)?;
+
+        let buf = tree_hash_cached(&a, puz, &HashSet::<NodePtr>::new(), &mut cache);
+        let puzzle_hash = a.new_atom(&buf)?;
+        process_single_spend::<MempoolVisitor>(
+            &a,
+            &mut ret,
+            &mut state,
+            parent,
+            puzzle_hash,
+            amount,
+            conditions,
+            flags,
+            &mut cost_left,
+            constants,
+        )?;
+    }
+
+    validate_conditions(&a, &ret, state, a.nil(), flags)?;
+    assert!(max_cost >= cost_left);
+    ret.cost = max_cost - cost_left;
+    let osbc = OwnedSpendBundleConditions::from(&a, ret);
+    Ok(osbc)
+}
---
+++ crates/chia-consensus/src/spendbundle_validation.rs
@@ -0,0 +1,38 @@
+use crate::consensus_constants::ConsensusConstants;
+use crate::gen::flags::{ALLOW_BACKREFS, DISALLOW_INFINITY_G1, ENABLE_MESSAGE_CONDITIONS};
+use crate::gen::opcodes::{
+    AGG_SIG_AMOUNT, AGG_SIG_ME, AGG_SIG_PARENT, AGG_SIG_PARENT_AMOUNT, AGG_SIG_PARENT_PUZZLE,
+    AGG_SIG_PUZZLE, AGG_SIG_PUZZLE_AMOUNT,
+};
+
+
+pub fn get_flags_for_height_and_constants(height: u32, constants: &ConsensusConstants) -> u32 {
+    let mut flags: u32 = 0;
+
+    if height >= constants.soft_fork4_height {
+        flags |= ENABLE_MESSAGE_CONDITIONS;
+    }
+
+    if height >= constants.soft_fork5_height {
+        flags |= DISALLOW_INFINITY_G1;
+    }
+
+    if height >= constants.hard_fork_height {
+        //  the hard-fork initiated with 2.0. To activate June 2024
+        //  * costs are ascribed to some unknown condition codes, to allow for
+        // soft-forking in new conditions with cost
+        //  * a new condition, SOFTFORK, is added which takes a first parameter to
+        //    specify its cost. This allows soft-forks similar to the softfork
+        //    operator
+        //  * BLS operators introduced in the soft-fork (behind the softfork
+        //    guard) are made available outside of the guard.
+        //  * division with negative numbers are allowed, and round toward
+        //    negative infinity
+        //  * AGG_SIG_* conditions are allowed to have unknown additional
+        //    arguments
+        //  * Allow the block generator to be serialized with the improved clvm
+        //   serialization format (with back-references)
+        flags = flags | ENABLE_BLS_OPS_OUTSIDE_GUARD | ENABLE_FIXED_DIV | ALLOW_BACKREFS;
+    }
+    flags
+}
