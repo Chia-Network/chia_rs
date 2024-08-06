41ffa0b8bf2402a7d9d9e569bc20f38361b43445
Rigidity <me@rigidnetwork.com>
2024-08-05 20:05:34 -0400

    Remove old stuff

--- crates/chia-client/src/error.rs
+++ crates/chia-client/src/error.rs
@@ -1,30 +1,11 @@
-use std::net::IpAddr;
-
 use chia_protocol::ProtocolMessageTypes;
-use semver::Version;
 use thiserror::Error;
-use tokio::sync::mpsc::error::SendError;
 use tokio::sync::oneshot::error::RecvError;
-use tokio::time::error::Elapsed;
-
-use crate::Event;
 
 #[derive(Debug, Error)]
 pub enum Error {
     #[error("Peer is missing certificate")]
     MissingCertificate,
-
-    #[error("Handshake not received")]
-    ExpectedHandshake,
-
-    #[error("Invalid protocol version {0}")]
-    InvalidProtocolVersion(String),
-
-    #[error("Wrong network id {0}")]
-    WrongNetworkId(String),
-
-    #[error("Outdated protocol version {0}, expected {1}")]
-    OutdatedProtocolVersion(Version, Version),
 
     #[error("Streamable error: {0}")]
     Streamable(#[from] chia_traits::Error),
@@ -35,9 +16,6 @@ pub enum Error {
     #[error("TLS error: {0}")]
     Tls(#[from] native_tls::Error),
 
-    #[error("Banned peer: {0}")]
-    BannedPeer(IpAddr),
-
     #[error("Unexpected message received with type {0:?}")]
     UnexpectedMessage(ProtocolMessageTypes),
 
@@ -47,17 +25,8 @@ pub enum Error {
     #[error("Failed to send event")]
     EventNotSent,
 
-    #[error("Failed to send message")]
-    Send(#[from] SendError<Event>),
-
     #[error("Failed to receive message")]
     Recv(#[from] RecvError),
-
-    #[error("Connection timeout: {0}")]
-    ConnectionTimeout(Elapsed),
-
-    #[error("Handshake timeout: {0}")]
-    HandshakeTimeout(Elapsed),
 
     #[error("IO error: {0}")]
     Io(#[from] std::io::Error),
--- crates/chia-client/src/lib.rs
+++ crates/chia-client/src/lib.rs
@@ -1,12 +1,10 @@
 mod error;
-mod event;
 mod network;
 mod peer;
 mod request_map;
 mod tls;
 
 pub use error::*;
-pub use event::*;
 pub use network::*;
 pub use peer::*;
 pub use tls::*;
