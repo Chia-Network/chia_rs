f316c7e4c0f860309d35b66aa8b8c9f6af49ab93
Matthew Howard <m.o.howard97@gmail.com>
2024-09-13 15:46:51 +0100

    clippy fix

--- crates/chia-protocol/src/spend_bundle.rs
+++ crates/chia-protocol/src/spend_bundle.rs
@@ -104,7 +104,7 @@ impl SpendBundle {
         spend_bundles: Vec<Self>,
     ) -> PyResult<PyObject> {
         let aggregated = Bound::new(py, Self::aggregate(&spend_bundles))?;
-        if aggregated.is_exact_instance(&cls) {
+        if aggregated.is_exact_instance(cls) {
             Ok(aggregated.into_py(py))
         } else {
             let instance = cls.call_method1("from_parent", (aggregated.into_py(py),))?;
