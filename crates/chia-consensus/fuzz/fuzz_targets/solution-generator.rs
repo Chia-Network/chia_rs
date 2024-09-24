faaba35f5455c6a0173c1b9363aba12752ccdc90
Arvid Norberg <arvid.norberg@gmail.com>
2024-09-24 10:08:54 +0200

    fixup fuzzer (#720)

--- crates/chia-consensus/fuzz/fuzz_targets/solution-generator.rs
+++ crates/chia-consensus/fuzz/fuzz_targets/solution-generator.rs
@@ -3,7 +3,7 @@ use chia_consensus::gen::solution_generator::{calculate_generator_length, soluti
 use chia_protocol::{Coin, CoinSpend};
 use chia_traits::Streamable;
 use clvmr::{
-    serde::{node_from_bytes, node_to_bytes},
+    serde::{node_from_bytes_backrefs, node_to_bytes},
     Allocator,
 };
 use libfuzzer_sys::fuzz_target;
@@ -13,7 +13,7 @@ fuzz_target!(|data: &[u8]| {
     let mut spends = Vec::<CoinSpend>::new();
     let mut generator_input = Vec::<(Coin, Vec<u8>, Vec<u8>)>::new();
     let mut data = Cursor::new(data);
-    let mut discrepancy: usize = 0;
+    let mut discrepancy: i64 = 0;
     let mut a = Allocator::new();
     while let Ok(spend) = CoinSpend::parse::<false>(&mut data) {
         spends.push(spend.clone());
@@ -23,30 +23,23 @@ fuzz_target!(|data: &[u8]| {
             spend.solution.to_vec(),
         ));
         // Check if puzzle or solution are atoms which can represented in a smaller form
-        let node = node_from_bytes(&mut a, spend.puzzle_reveal.as_ref()).expect("node");
-        if node.is_atom() {
-            let puz = node_to_bytes(&a, node).expect("bytes");
-            discrepancy += spend.puzzle_reveal.as_ref().len() - puz.len();
-        }
-        let node = node_from_bytes(&mut a, spend.solution.as_ref()).expect("node");
-        if node.is_atom() {
-            let sol = node_to_bytes(&a, node).expect("bytes");
-            discrepancy += spend.solution.as_ref().len() - sol.len();
-        }
+        let node = node_from_bytes_backrefs(&mut a, spend.puzzle_reveal.as_ref()).expect("node");
+        let puz = node_to_bytes(&a, node).expect("bytes");
+        discrepancy += spend.puzzle_reveal.as_ref().len() as i64 - puz.len() as i64;
+        let node = node_from_bytes_backrefs(&mut a, spend.solution.as_ref()).expect("node");
+        let sol = node_to_bytes(&a, node).expect("bytes");
+        discrepancy += spend.solution.as_ref().len() as i64 - sol.len() as i64;
     }
     if spends.is_empty() {
         return;
     }
-    let Ok(result) = solution_generator(generator_input) else {
-        return;
-    };
-
-    if result.len() != calculate_generator_length(spends.clone()) - discrepancy {
+    let result = solution_generator(generator_input).expect("solution_generator");
+    if result.len() as i64 != calculate_generator_length(spends.clone()) as i64 - discrepancy {
         panic!("Debug spends: {:?}", spends);
     }
 
     assert_eq!(
-        result.len(),
-        calculate_generator_length(spends) - discrepancy
+        result.len() as i64,
+        calculate_generator_length(spends) as i64 - discrepancy
     );
 });
