19ca4233518f47bcfb50246f4d8a444ef637f677
Matthew Howard <m.o.howard97@gmail.com>
2025-01-17 15:46:13 +0000

    black tests

--- tests/test_blscache.py
+++ tests/test_blscache.py
@@ -297,11 +297,7 @@ def test_validate_clvm_and_sig():
     )
     sig = AugSchemeMPL.sign(
         sk,
-        (
-            b'hello'
-            + coin.name()
-            + DEFAULT_CONSTANTS.AGG_SIG_ME_ADDITIONAL_DATA
-        ),  # noqa
+        (b"hello" + coin.name() + DEFAULT_CONSTANTS.AGG_SIG_ME_ADDITIONAL_DATA),  # noqa
     )
 
     new_spend = SpendBundle(coin_spends, sig)
