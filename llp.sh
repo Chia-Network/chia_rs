d3e6c707ccc62f45e50ee9d23c2c2e18784c686c
Kyle Altendorf <sda@fstab.net>
2025-01-30 09:31:50 -0500

    Update llp.sh

--- llp.sh
+++ llp.sh
@@ -1,3 +1,3 @@
 # TODO: avoid needing this
 
-export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:$(python -c 'import sysconfig; print(sysconfig.get_config_var("LIBDIR"))')
+#export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:$(python -c 'import sysconfig; print(sysconfig.get_config_var("LIBDIR"))')
