a3c2346714bb114ae6fb78e79068ff44725a296e
Matthew Howard <m.o.howard97@gmail.com>
2025-05-12 12:15:54 +0100

    add COST_CONDITIONS flag to get_flags_for_height_and_constants() after hard fork 2

--- crates/chia-consensus/src/spendbundle_validation.rs
+++ crates/chia-consensus/src/spendbundle_validation.rs
@@ -1,5 +1,6 @@
 use crate::allocator::make_allocator;
 use crate::consensus_constants::ConsensusConstants;
+use crate::r#gen::flags::COST_CONDITIONS;
 use crate::gen::owned_conditions::OwnedSpendBundleConditions;
 use crate::gen::validation_error::ErrorCode;
 use crate::spendbundle_conditions::run_spendbundle;
@@ -81,7 +82,7 @@ pub fn get_flags_for_height_and_constants(height: u32, constants: &ConsensusCons
     // In hard fork 2, we enable the keccak operator outside the softfork guard
     let mut flags: u32 = 0;
     if height >= constants.hard_fork2_height {
-        flags |= ENABLE_KECCAK_OPS_OUTSIDE_GUARD;
+        flags |= ENABLE_KECCAK_OPS_OUTSIDE_GUARD | COST_CONDITIONS;
     }
     flags
 }
