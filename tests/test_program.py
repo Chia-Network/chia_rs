fe20d3dd6ffdd02968684c7a63c6c257c7325afc
Matthew Howard <m.o.howard97@gmail.com>
2025-05-23 11:18:48 +0100

    re-add program run test_raise

--- tests/test_program.py
+++ tests/test_program.py
@@ -13,6 +13,16 @@ def test_raise() -> None:
     except ValueError as e:
         assert f"{e}" == "('clvm raise', '86666f6f626172')"
 
+def test_raise_program() -> None:
+    try:
+        # (x (q . "foobar"))
+        prg = Program.fromhex("ff08ffff0186666f6f62617280")
+
+        prg.run_rust(100000, 0, [])
+        # We expect this to throw
+        assert False
+    except ValueError as e:
+        assert f"{e}" == "('clvm raise', '86666f6f626172')"
 
 def test_repr() -> None:
     temp = Program.to([8, (1, "foo")])
