a714987408976ec132809f70ac5defef44b8ad42
Kyle Altendorf <sda@fstab.net>
2025-05-28 14:22:01 -0400

    copied

--- crates/chia-datalayer/src/merkle.rs
+++ crates/chia-datalayer/src/merkle.rs
@@ -3870,7 +3870,7 @@ mod tests {
     #[test]
     fn test_delta_reader_create_merkle_blob_works() {
         let mut delta_reader = complete_delta_reader();
-        let interested_hashes: HashSet<Hash> = delta_reader.nodes.keys().cloned().collect();
+        let interested_hashes: HashSet<Hash> = delta_reader.nodes.keys().copied().collect();
 
         let (complete_blob, _) = delta_reader
             .create_merkle_blob_and_filter_unused_nodes(HASH_ZERO, &interested_hashes)
