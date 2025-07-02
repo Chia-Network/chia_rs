8f052ee74a4c1d3f99175a1e9dc8c574e1c235e5
Matthew Howard <m.o.howard97@gmail.com>
2025-07-02 07:48:04 +0100

    missed count reference

--- wheel/src/api.rs
+++ wheel/src/api.rs
@@ -637,7 +637,7 @@ pub fn get_spends_for_trusted_block_with_conditions<'a>(
                     break; // we only care about the first 5 conditions
                 }
             }
-            if count > 0 {
+            if bytes_vec.len() > 0 {
                 // we have a valid condition
                 cond_output.push((num, bytes_vec));
             }
