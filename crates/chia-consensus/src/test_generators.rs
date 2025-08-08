bb528c0c767f70c05a76894d54266fda5fea8144
Matthew Howard <m.o.howard97@gmail.com>
2025-08-08 11:07:54 +0100

    UPDATE_TEST = false

--- crates/chia-consensus/src/test_generators.rs
+++ crates/chia-consensus/src/test_generators.rs
@@ -224,7 +224,7 @@ fn run_generator(#[case] name: &str) {
     // When making changes to print_conditions() enabling this will update the
     // test cases to match. Make sure to carefully review the diff before
     // landing an automatic update of the test case.
-    const UPDATE_TESTS: bool = true;
+    const UPDATE_TESTS: bool = false;
 
     let filename = format!("../../generator-tests/{name}.txt");
     println!("file: {filename}");
