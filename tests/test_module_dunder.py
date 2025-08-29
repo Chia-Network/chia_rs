6f14a03ba2222a17e2ed66b60c01473dba32a678
Kyle Altendorf <sda@fstab.net>
2025-08-28 21:20:01 -0400

    add test

---
+++ tests/test_module_dunder.py
@@ -0,0 +1,38 @@
+from types import ModuleType
+from typing import Optional
+
+import pytest
+
+import chia_rs
+
+
+def recurse_module(module: ModuleType, ignore: set[str], prefix: Optional[str] = None, top_level: Optional[ModuleType] = None):
+    if prefix is None:
+        prefix = module.__name__
+    if top_level is None:
+        top_level = module
+
+    for name, value in vars(module).items():
+        if name.startswith("_"):
+            continue
+
+        full_name = f"{prefix}.{name}"
+        if full_name in ignore:
+            continue
+
+        dunder_module = getattr(value, "__module__", None)
+        if dunder_module is not None:
+            yield (full_name, prefix, dunder_module)
+        if (
+            isinstance(value, ModuleType)
+            and module.__name__.startswith(f"{top_level.__name__}.")
+        ):
+            yield from f(module=module, ignore=ignore, prefix=full_name, top_level=top_level)
+
+
+@pytest.mark.parametrize(
+    argnames=["full_name", "prefix", "dunder_module"],
+    argvalues=recurse_module(module=chia_rs, ignore={"chia_rs.chia_rs"}),
+)
+def test_it(full_name, prefix, dunder_module) -> None:
+    assert dunder_module == prefix, f"failing for: {full_name}"
