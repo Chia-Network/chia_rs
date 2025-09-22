d42bc219be8cfdfce01e748bf20f1ddf3277c474
Matthew Howard <m.o.howard97@gmail.com>
2025-09-22 13:12:22 +0100

    further lower costs

--- crates/chia-consensus/src/consensus_constants.rs
+++ crates/chia-consensus/src/consensus_constants.rs
@@ -250,6 +250,6 @@ pub const TEST_CONSTANTS: ConsensusConstants = ConsensusConstants {
     plot_filter_v2_second_adjustment_height: 0xffff_ffff, // placeholder
     plot_filter_v2_third_adjustment_height: 0xffff_ffff, // placeholder
 
-    shatree_recurse_cost: 1000,
-    shatree_cost_per_byte: 500,
+    shatree_recurse_cost: 500,
+    shatree_cost_per_byte: 250,
 };
