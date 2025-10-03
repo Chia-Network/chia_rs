ac822f3b3ded5745014dcc66bb1ddb271facf5dc
Matthew Howard <m.o.howard97@gmail.com>
2025-10-03 12:05:05 +0100

    regenerate stubs

--- wheel/python/chia_rs/chia_rs.pyi
+++ wheel/python/chia_rs/chia_rs.pyi
@@ -4466,8 +4466,6 @@ class ConsensusConstants:
     PLOT_FILTER_V2_FIRST_ADJUSTMENT_HEIGHT: uint32
     PLOT_FILTER_V2_SECOND_ADJUSTMENT_HEIGHT: uint32
     PLOT_FILTER_V2_THIRD_ADJUSTMENT_HEIGHT: uint32
-    SHATREE_RECURSE_COST: uint64
-    SHATREE_COST_PER_BYTE: uint64
     def __new__(
         cls,
         SLOT_BLOCKS_TARGET: uint32,
@@ -4523,9 +4521,7 @@ class ConsensusConstants:
         QUALITY_PROOF_SCAN_FILTER: uint8,
         PLOT_FILTER_V2_FIRST_ADJUSTMENT_HEIGHT: uint32,
         PLOT_FILTER_V2_SECOND_ADJUSTMENT_HEIGHT: uint32,
-        PLOT_FILTER_V2_THIRD_ADJUSTMENT_HEIGHT: uint32,
-        SHATREE_RECURSE_COST: uint64,
-        SHATREE_COST_PER_BYTE: uint64
+        PLOT_FILTER_V2_THIRD_ADJUSTMENT_HEIGHT: uint32
     ) -> ConsensusConstants: ...
     def __hash__(self) -> int: ...
     def __repr__(self) -> str: ...
@@ -4597,6 +4593,4 @@ class ConsensusConstants:
         QUALITY_PROOF_SCAN_FILTER: Union[ uint8, _Unspec] = _Unspec(),
         PLOT_FILTER_V2_FIRST_ADJUSTMENT_HEIGHT: Union[ uint32, _Unspec] = _Unspec(),
         PLOT_FILTER_V2_SECOND_ADJUSTMENT_HEIGHT: Union[ uint32, _Unspec] = _Unspec(),
-        PLOT_FILTER_V2_THIRD_ADJUSTMENT_HEIGHT: Union[ uint32, _Unspec] = _Unspec(),
-        SHATREE_RECURSE_COST: Union[ uint64, _Unspec] = _Unspec(),
-        SHATREE_COST_PER_BYTE: Union[ uint64, _Unspec] = _Unspec()) -> ConsensusConstants: ...
+        PLOT_FILTER_V2_THIRD_ADJUSTMENT_HEIGHT: Union[ uint32, _Unspec] = _Unspec()) -> ConsensusConstants: ...
