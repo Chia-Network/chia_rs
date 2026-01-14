cdbf2dc42730a0ab372e3b6e174fb72aff7e0661
Rigidity <me@rigidnetwork.com>
2026-01-14 15:37:36 -0500

    Make pos2 dependency optional on chia-protocol

--- crates/chia-protocol/src/partial_proof.rs
+++ crates/chia-protocol/src/partial_proof.rs
@@ -1,10 +1,12 @@
 use crate::Bytes32;
-use chia_pos2::serialize_quality;
 use chia_sha2::Sha256;
 use chia_streamable_macro::streamable;
 
 #[cfg(feature = "py-bindings")]
 use pyo3::pymethods;
+
+#[cfg(feature = "py-bindings")]
+use chia_pos2::serialize_quality;
 
 #[streamable]
 pub struct PartialProof {
@@ -12,19 +14,13 @@ pub struct PartialProof {
     fragments: [u64; 16],
 }
 
-impl PartialProof {
-    pub fn get_string(&self, strength: u8) -> Bytes32 {
-        let mut sha256 = Sha256::new();
-        sha256.update(serialize_quality(&self.fragments, strength));
-        sha256.finalize().into()
-    }
-}
-
 #[cfg(feature = "py-bindings")]
 #[pymethods]
 impl PartialProof {
     #[pyo3(name = "get_string")]
     fn py_get_string(&self, strength: u8) -> Bytes32 {
-        self.get_string(strength)
+        let mut sha256 = Sha256::new();
+        sha256.update(serialize_quality(&self.fragments, strength));
+        sha256.finalize().into()
     }
 }
