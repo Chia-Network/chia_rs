eee2226a5ca6a4f8bf0a4c4494939463e4250e28
arvidn <arvid@libtorrent.org>
2026-01-30 12:58:15 +0100

    convert data-layer fuzzers to be structure-aware

--- crates/chia-datalayer/fuzz/fuzz_targets/proofs_of_inclusion.rs
+++ crates/chia-datalayer/fuzz/fuzz_targets/proofs_of_inclusion.rs
@@ -1,27 +1,18 @@
 #![no_main]
 
-use libfuzzer_sys::{
-    arbitrary::{Arbitrary, Unstructured},
-    fuzz_target,
-};
-
 use chia_datalayer::{Error, Hash, InsertLocation, KeyId, MerkleBlob, ValueId};
+use libfuzzer_sys::fuzz_target;
 
-fuzz_target!(|data: &[u8]| {
-    let mut blob = MerkleBlob::new(Vec::new()).unwrap();
+fuzz_target!(|args: Vec<(KeyId, ValueId, Hash)>| {
+    let mut blob = MerkleBlob::new(Vec::new()).expect("construct MerkleBlob");
     blob.check_integrity_on_drop = false;
 
     let mut keys: Vec<KeyId> = Vec::new();
 
-    let mut unstructured = Unstructured::new(data);
-    while !unstructured.is_empty() {
-        let key = KeyId::arbitrary(&mut unstructured).unwrap();
-        let value = ValueId::arbitrary(&mut unstructured).unwrap();
-        let hash = Hash::arbitrary(&mut unstructured).unwrap();
-
-        match blob.insert(key, value, &hash, InsertLocation::Auto {}) {
+    for (key, value, hash) in &args {
+        match blob.insert(*key, *value, hash, InsertLocation::Auto {}) {
             Ok(_) => {
-                keys.push(key);
+                keys.push(*key);
             }
             // should remain valid through these errors
             Err(Error::KeyAlreadyPresent()) => continue,
